64d9a94c92fdb758d088cefdd87ee8c9af9ea976
Francis Yu Yan <yuyan0@stanford.edu>
2016-04-04 13:23:29 -0700

    add default-tcp

--- src/default-tcp/tcp-server.c
+++ src/default-tcp/tcp-server.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/poll.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -32,15 +33,47 @@ int main(int argc, char** argv) {
       (struct sockaddr*)&remoteAddr, &remoteAddrLen);
   printf("Connected to client\n");
 
-  char recv_buf[MAXDATASIZE + 1];
+  char send_buf[MAXDATASIZE];
+  char recv_buf[MAXDATASIZE];
+
+  struct pollfd ufds[2];
+  ufds[0].fd = STDIN_FILENO;
+  ufds[0].events = POLLIN; 
+
+  ufds[1].fd = remoteSocket;
+  ufds[1].events = POLLIN; 
+
+  int rv;
   while (1) {
-    int recv_bytes = recv(remoteSocket, recv_buf, MAXDATASIZE, 0);
-    if (recv_bytes > 0) {
-      recv_buf[recv_bytes] = '\0';
-      printf("%s", recv_buf);
-    } else if (recv_bytes == 0) {
-      printf("Client sent 0 bytes\n");
-      break;
+    rv = poll(ufds, 2, 10000);
+    if (rv == -1) {
+      perror("poll");
+    } else if (rv == 0) {
+      printf("No data after 10 seconds timeout");
+    } else {
+      if (ufds[0].revents & POLLIN) {
+        if (fgets(send_buf, MAXDATASIZE - 1, stdin)) {
+          int sent_bytes = send(remoteSocket, send_buf, strlen(send_buf), 0);
+          if (sent_bytes < 0)
+            perror("send");
+          else if (sent_bytes != strlen(send_buf))
+            printf("Partially sent\n");
+        } else {
+          send(localSocket, send_buf, 0, 0);
+          break;
+        }
+      }
+
+      if (ufds[1].revents & POLLIN) {
+        int recv_bytes = recv(remoteSocket, recv_buf, MAXDATASIZE - 1, 0);
+        if (recv_bytes > 0) {
+          recv_buf[recv_bytes] = '\0';
+          printf("%s", recv_buf);
+        } else if (recv_bytes == 0) {
+          printf("The other side has closed connection\n");
+          break;
+        }
+      }
     }
   }
 
