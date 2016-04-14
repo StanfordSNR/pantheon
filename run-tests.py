5050a38243414cf2b05a7075b3b5b65599f87a08
Francis <yuyan0@stanford.edu>
2016-04-14 14:16:50 -0700

    generate large file

--- run-tests.py
+++ run-tests.py
@@ -1,7 +1,13 @@
-import sys
+import os, sys
 
+def generate_large_file():
+    if not os.path.isfile('./test/random-input'):
+        with open('./test/random-input', 'w') as f:
+            f.write(os.urandom(1024 * 1024 * 1024))
+    
 def main():
-   print "Test run-tests.py" 
+    generate_large_file() 
+    print "Test run-tests.py" 
 
 if __name__ == '__main__':
     main()
--- test/default-tcp-test.py
+++ test/default-tcp-test.py
@@ -7,8 +7,11 @@ def main():
     setup_proc = subprocess.Popen(setup_cmd, cwd = path, shell = True)
     setup_proc.communicate()
 
-    recv_cmd = 'python default-tcp.py receiver &'
-    recv_proc = subprocess.Popen(recv_cmd, cwd = path, shell = True)
+    port_proc = subprocess.Popen('./find-unused-port', cwd = path, shell = True)
+    port = port_proc.communicate()[0]
+
+    recv_cmd = 'python default-tcp.py receiver port < '
+    recv_proc = subprocess.Popen(recv_cmd, cwd = path
 
     '''
     while True:
