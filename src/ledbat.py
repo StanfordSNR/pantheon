f3aeea504b62708556f2cef8212acd965d9a6bf0
Francis <yuyan0@stanford.edu>
2016-04-20 11:10:22 -0700

    use ucat-static

--- src/ledbat.py
+++ src/ledbat.py
@@ -9,28 +9,33 @@ def main():
     option = sys.argv[1]
 
     if option == 'setup':
+        if len(sys.argv) != 2: 
+            general_usage()
+            return
         sys.stderr.write("Setup done.\n")
 
     if option == 'receiver':
-        if len(sys.argv) == 3:
-            port = sys.argv[2]
-        else:  
-            proc = subprocess.Popen(['./find_unused_port'], stdout = subprocess.PIPE)
-            port = proc.communicate()[0]  
+        if len(sys.argv) != 2: 
+            general_usage()
+            return
+
+        proc = subprocess.Popen(['find_unused_port'], stdout = subprocess.PIPE)
+        port = proc.communicate()[0]  
         sys.stderr.write("Listening on port: %s\n" % port)
 
-        cmd = ['./ucat', '-l', '-p', port]
-        path = '../external/libutp'
+        cmd = ['./ucat-static', '-l', '-p', port]
+        path = '../third_party/libutp'
         subprocess.call(cmd, cwd = path)
 
     if option == 'sender':
-        if len(sys.argv) < 4:
+        if len(sys.argv) != 4:
             general_usage()
             return
+
         ip = sys.argv[2]
         port = sys.argv[3] 
-        cmd = ['./ucat', ip, port]
-        path = '../external/libutp'
+        cmd = ['./ucat-static', ip, port]
+        path = '../third_party/libutp'
         subprocess.call(cmd, cwd = path)
     
 if __name__ == '__main__':
