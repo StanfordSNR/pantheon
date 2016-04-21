78ec47344f8f5d85ec03f3ac73a700abe38f4d60
Francis <yuyan0@stanford.edu>
2016-04-21 01:13:53 -0700

    test.py: run 10s and terminate all

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import os, sys
 from subprocess import Popen, PIPE
-import filecmp, tempfile
+from interruptingcow import timeout
 import unittest
 
 # print test usage
@@ -15,46 +15,35 @@ def parse_cc_option(cc_option):
     # add more congestion control options here
     if cc_option == 'ledbat':
         return 'ledbat.py'
+    if cc_option == 'default_tcp':
+        return 'default_tcp.py'
 
     print "Congestion control option is not valid."
     return None
 
-# generate tmp file of 'size' (MB)
-def generate_tmp_file(size):
-    f = tempfile.NamedTemporaryFile(delete=False) 
-    f.write(os.urandom(1024 * 1024 * size))
-    f.close()
-    return f.name
-
 class TestCongestionControl(unittest.TestCase):
     # run parameterized test cases
-    def __init__(self, test_name, src_file):
+    def __init__(self, test_name, src_fname):
         super(TestCongestionControl, self).__init__(test_name)
-        self.src_file = src_file
+        self.src_fname = src_fname
 
     # congestion control test
     def test_congestion_control(self):
-        src_file = self.src_file
+        src_fname = self.src_fname
+        src_path = os.path.join(os.path.dirname(__file__), '../src') 
+        src_file = os.path.join(src_path, src_fname) 
 
         # open /dev/null to ignore some output
         DEVNULL = open(os.devnull, 'wb')
 
-        # generate input file of a random name
-        input_fname = generate_tmp_file(1) 
-        input_file = open(input_fname, 'rb')
-
-        # open output file of a random name
-        output_file = tempfile.NamedTemporaryFile(delete=False)
-        output_fname = output_file.name
-
         # run setup, ignore any output
         setup_cmd = ['python', src_file, 'setup'] 
         setup_proc = Popen(setup_cmd, stdout=DEVNULL, stderr=DEVNULL)
-        setup_proc.wait()
+        setup_proc.communicate()
         
         # run receiver, which outputs to output_file 
         recv_cmd = ['python', src_file, 'receiver'] 
-        recv_proc = Popen(recv_cmd, stdout=output_file, stderr=PIPE)
+        recv_proc = Popen(recv_cmd, stdout=DEVNULL, stderr=PIPE)
 
         # find port that receiver prints
         port_info = recv_proc.stderr.readline()
@@ -63,26 +52,32 @@ class TestCongestionControl(unittest.TestCase):
         # run sender, which gets input from input_file 
         ip = '127.0.0.1'
         send_cmd = ['python', src_file, 'sender', ip, port]
-        send_proc = Popen(send_cmd, stdin=input_file, 
-                    stdout=DEVNULL, stderr=DEVNULL)
 
-        # wait until the sender and receiver finish transferring 
-        send_proc.communicate()
-        recv_proc.communicate()
+        success = False
+        if src_fname == 'default_tcp.py':
+            send_proc = Popen(send_cmd, stdout=DEVNULL, stderr=DEVNULL)
+            send_proc.communicate()
+            recv_proc.terminate()
+            success = True 
+        else:
+            send_proc = Popen(send_cmd, stdin=PIPE, 
+                        stdout=DEVNULL, stderr=DEVNULL)
+            # write random stuff to sender for 10 seconds
+            try:
+                with timeout(10, exception=RuntimeError):
+                    while True:
+                        send_proc.stdin.write(os.urandom(1024 * 128))
+            except:
+                success = True
+
+            send_proc.stdin.close()
+            send_proc.terminate()
+            recv_proc.terminate()
 
         # close files
         DEVNULL.close()
-        input_file.close()
-        output_file.close()
 
-        # compare the files sent and received 
-        diff = filecmp.cmp(input_fname, output_fname)
-
-        # remove generated files anyway
-        os.unlink(input_fname)
-        os.unlink(output_fname)
-
-        self.assertTrue(diff)
+        self.assertTrue(success)
 
 def main():
     if len(sys.argv) != 2:
@@ -94,12 +89,10 @@ def main():
     src_fname = parse_cc_option(cc_option)
     if src_fname is None:
         return
-    src_path = os.path.join(os.path.dirname(__file__), '../src') 
-    src_file = os.path.join(src_path, src_fname) 
 
     # create test suite to run
     suite = unittest.TestSuite()
-    suite.addTest(TestCongestionControl("test_congestion_control", src_file))
+    suite.addTest(TestCongestionControl("test_congestion_control", src_fname))
     unittest.TextTestRunner().run(suite)
 
 if __name__ == '__main__':
