a7b213b60043f713012596791894840c9b2dfca8
Francis <yuyan0@stanford.edu>
2016-04-21 01:13:12 -0700

    use iperf as default_tcp; run 10s for now

---
+++ src/default_tcp.py
@@ -0,0 +1,49 @@
+import os, sys, subprocess
+from subprocess import Popen, PIPE
+from usage import general_usage
+
+def main():
+    script_path = os.path.dirname(__file__)
+    find_unused_port_file = os.path.join(script_path, 'find_unused_port')
+    src_file = 'iperf'
+
+    if len(sys.argv) < 2:
+        general_usage()
+        return
+
+    cc_option = sys.argv[1]
+
+    # setup
+    if cc_option == 'setup':
+        if len(sys.argv) != 2: 
+            general_usage()
+            return
+        sys.stderr.write("Setup done.\n")
+
+    # receiver
+    if cc_option == 'receiver':
+        if len(sys.argv) != 2: 
+            general_usage()
+            return
+
+        proc = Popen([find_unused_port_file], stdout=PIPE)
+        port = proc.communicate()[0]  
+        sys.stderr.write("Listening on port: %s\n" % port)
+
+        cmd = [src_file, '-s', '-p', port, '-t', '10']
+        subprocess.call(cmd)
+
+    # sender
+    if cc_option == 'sender':
+        if len(sys.argv) != 4:
+            general_usage()
+            return
+
+        ip = sys.argv[2]
+        port = sys.argv[3] 
+        cmd = [src_file, '-c', ip, '-p', port, '-t', '10']
+        subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+    main()
