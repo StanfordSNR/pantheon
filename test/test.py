eb3f6e1536afec080e96f4c5836c6adc739ebfcc
Francis <yuyan0@stanford.edu>
2016-04-24 13:14:22 -0700

    add QUIC test

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
-import os, sys, tempfile, subprocess
+import os, sys, subprocess, time
 from subprocess import Popen, PIPE
 from interruptingcow import timeout
+from generate_html import generate_html
 import unittest
 
 # print test usage
@@ -11,11 +12,14 @@ def test_usage():
 # parse congestion control option, return friendly name and script to run
 def parse_cc_option(cc_option):
     # add more congestion control options here
+    if cc_option == 'default_tcp':
+        return 'Default TCP', 'default_tcp.py'
+
     if cc_option == 'ledbat':
         return 'LEDBAT', 'ledbat.py'
 
-    if cc_option == 'default_tcp':
-        return 'Default TCP', 'default_tcp.py'
+    if cc_option == 'quic':
+        return 'QUIC', 'quic.py'
 
     print "Congestion control option is not valid."
     return None, None
@@ -31,75 +35,141 @@ class TestCongestionControl(unittest.TestCase):
         super(TestCongestionControl, self).__init__(test_name)
         self.cc_friendly_name = cc_friendly_name
         self.src_name = src_name
+        self.DEVNULL = open(os.devnull, 'wb')
 
-    # congestion control test
-    def test_congestion_control(self):
-        cc_friendly_name = self.cc_friendly_name
-        src_name = self.src_name
-        cc_name = src_name[:-3]
-        test_dir = os.path.dirname(__file__)
-        src_dir = os.path.join(test_dir, '../src') 
-        src_file = os.path.join(src_dir, src_name) 
+    def __del__(self):
+        self.DEVNULL.close()
 
-        # open /dev/null to ignore some output
-        DEVNULL = open(os.devnull, 'wb')
-
-        # run setup, ignore any output
-        setup_cmd = ['python', src_file, 'setup'] 
-        setup_proc = Popen(setup_cmd, stdout=DEVNULL, stderr=DEVNULL)
-        setup_proc.communicate()
-        
+    # Default TCP
+    def run_default_tcp(self):
         # run receiver
-        recv_cmd = ['python', src_file, 'receiver'] 
-        recv_proc = Popen(recv_cmd, stdout=DEVNULL, stderr=PIPE)
+        recv_cmd = ['python', self.src_file, 'receiver'] 
+        recv_proc = Popen(recv_cmd, stdout=self.DEVNULL, stderr=PIPE)
 
         # find port that receiver prints
         port_info = recv_proc.stderr.readline()
         port = port_info.rstrip().rsplit(' ', 1)[1]
 
-        # run sender in mahimahi
-        traces_dir = '/usr/share/mahimahi/traces/'
-        uplink_trace = traces_dir + 'Verizon-LTE-short.up'
-        downlink_trace = traces_dir + 'Verizon-LTE-short.down'
-        uplink_log = os.path.join(test_dir, '%s_uplink.log' % cc_name) 
-        
-        ip = '$MAHIMAHI_BASE'
-        send_cmd = "'python %s sender %s %s'" % (src_file, ip, port)
+        # run sender
+        send_cmd = "'python %s sender %s %s'" % (self.src_file, self.ip, port)
         mahimahi_cmd = 'mm-link %s %s --once --uplink-log=%s -- sh -c %s' % \
-                        (uplink_trace, downlink_trace, uplink_log, send_cmd) 
+                        (self.uplink_trace, self.downlink_trace,
+                         self.link_log, send_cmd) 
 
-        print 'Running %s...' % cc_friendly_name 
-        if src_name == 'default_tcp.py':
-            # default_tcp.py runs for 10 seconds itself
-            send_proc = Popen(mahimahi_cmd, shell=True, 
-                        stdout=DEVNULL, stderr=DEVNULL)
-            send_proc.communicate()
-            recv_proc.terminate()
+        send_proc = Popen(mahimahi_cmd, shell=True, 
+                    stdout=self.DEVNULL, stderr=self.DEVNULL)
+        try:
+            with timeout(10, exception=RuntimeError):
+                send_proc.communicate()
+        except:
+            pass
         else:
-            send_proc = Popen(mahimahi_cmd, shell=True, 
-                        stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
-            # other schemes require writing random data to sender for 10 seconds
-            try:
-                with timeout(10, exception=RuntimeError):
-                    while True:
-                        send_proc.stdin.write(os.urandom(1024 * 128))
-            except:
-                pass
-            else:
-                print 'Warning: quit earlier!'
+            print 'Warning: quit earlier!'
 
-            send_proc.stdin.close()
-            send_proc.terminate()
-            recv_proc.terminate()
+        send_proc.terminate()
+        recv_proc.terminate()
+
+    # LEDBAT
+    def run_ledbat(self):
+        # run receiver
+        recv_cmd = ['python', self.src_file, 'receiver'] 
+        recv_proc = Popen(recv_cmd, stdout=self.DEVNULL, stderr=PIPE)
+
+        # find port that receiver prints
+        port_info = recv_proc.stderr.readline()
+        port = port_info.rstrip().rsplit(' ', 1)[1]
+
+        # run sender
+        send_cmd = "'python %s sender %s %s'" % (self.src_file, self.ip, port)
+        mahimahi_cmd = 'mm-link %s %s --once --uplink-log=%s -- sh -c %s' % \
+                        (self.uplink_trace, self.downlink_trace,
+                         self.link_log, send_cmd) 
+
+        # writing random data to sender for 10 seconds
+        send_proc = Popen(mahimahi_cmd, shell=True, 
+                    stdin=PIPE, stdout=self.DEVNULL, stderr=self.DEVNULL)
+        try:
+            with timeout(10, exception=RuntimeError):
+                while True:
+                    send_proc.stdin.write(os.urandom(1024 * 128))
+        except:
+            pass
+        else:
+            print 'Warning: quit earlier!'
+
+        send_proc.stdin.close()
+        send_proc.terminate()
+        recv_proc.terminate()
+
+    # QUIC
+    def run_quic(self):
+        # generate html of size that can be transferred longer than 10 seconds 
+        generate_html(300000)
+
+        # run sender
+        send_cmd = 'python %s sender' % self.src_file
+        send_proc = Popen(send_cmd, shell=True, stdout=self.DEVNULL, stderr=PIPE)
+
+        # find port that sender prints
+        port_info = send_proc.stderr.readline()
+        port = port_info.rstrip().rsplit(' ', 1)[1]
+
+        # run receiver (notice direction: switch uplink and downlink trace)
+        recv_cmd = "'python %s receiver %s %s > /dev/null'" % (self.src_file, self.ip, port)
+        mahimahi_cmd = 'mm-link %s %s --once --downlink-log=%s -- sh -c %s' % \
+                        (self.downlink_trace, self.uplink_trace,
+                         self.link_log, recv_cmd) 
+
+        recv_proc = Popen(mahimahi_cmd, shell=True, 
+                          stdout=self.DEVNULL, stderr=self.DEVNULL)
+
+        try:
+            with timeout(10, exception=RuntimeError):
+                send_proc.communicate()
+        except:
+            pass
+        else:
+            print 'Warning: quit earlier!'
+
+        send_proc.terminate()
+        recv_proc.terminate()
+
+    # congestion control test
+    def test_congestion_control(self):
+        src_name = self.src_name
+        cc_prefix = src_name[:-3]
+        test_dir = os.path.abspath(os.path.dirname(__file__))
+        src_dir = os.path.abspath(os.path.join(test_dir, '../src')) 
+        self.src_file = os.path.join(src_dir, src_name) 
+
+        # run setup, ignore any output
+        setup_cmd = ['python', self.src_file, 'setup'] 
+        setup_proc = Popen(setup_cmd, stdout=self.DEVNULL, stderr=self.DEVNULL)
+        setup_proc.communicate()
+        
+        # prepare mahimahi
+        traces_dir = '/usr/share/mahimahi/traces/'
+        self.uplink_trace = traces_dir + 'Verizon-LTE-short.up'
+        self.downlink_trace = traces_dir + 'Verizon-LTE-short.down'
+        self.link_log = os.path.join(test_dir, '%s_link.log' % cc_prefix) 
+        self.ip = '$MAHIMAHI_BASE'
+
+        print 'Running %s...' % self.cc_friendly_name 
+        # add more congestion control mechanisms tests here
+        if src_name == 'default_tcp.py':
+            self.run_default_tcp()
+
+        if src_name == 'ledbat.py':
+            self.run_ledbat()
+
+        if src_name == 'quic.py':
+            self.run_quic()
 
         # generate throughput graph
         throughput_graph_file = open(os.path.join(test_dir, 
-                                '%s_throughput.html' % cc_name), 'wb')
-        subprocess.call(['mm-throughput-graph', '500', uplink_log],
+                                '%s_throughput.html' % cc_prefix), 'wb')
+        subprocess.call(['mm-throughput-graph', '500', self.link_log],
                         stdout=throughput_graph_file)
-
-        # close / remove files
-        DEVNULL.close()
         throughput_graph_file.close()
 
 def main():
