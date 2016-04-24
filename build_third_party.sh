805887caa1573cff0482a4445b061786edba4b02
Francis <yuyan0@stanford.edu>
2016-04-24 13:06:50 -0700

    add proto-quic submodule

--- build_third_party.sh
+++ build_third_party.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-cd third_party
+proj_dir=$( cd $(dirname $0) ; pwd )
+third_party_dir=$proj_dir/third_party
 
 # build libutp
-cd libutp
+cd $third_party_dir/libutp
 make
-cd ..
 
-cd ..
+# build proto-quic
+cd $third_party_dir/proto-quic
+export PATH=$PATH:`pwd`/depot_tools
+./src/build/install-build-deps.sh --no-prompt
+
+cd $third_party_dir/proto-quic/src
+gclient runhooks && ninja -C out/Release quic_client quic_server net_unittests
