ee43791919792c579e2e49d6b993ebfcbb6779a1
Francis <yuyan0@stanford.edu>
2016-05-04 15:41:51 -0700

    remove bash -e

--- build_third_party.sh
+++ build_third_party.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/sh -x
 
 proj_dir=$( cd $(dirname $0) ; pwd )
 third_party_dir=$proj_dir/third_party
@@ -14,8 +14,8 @@ cd $third_party_dir/pcc/receiver
 make
 
 # build proto-quic
-#cd $third_party_dir/proto-quic
-#export PATH=$PATH:`pwd`/depot_tools
-#cd $third_party_dir/proto-quic/src
-#./build/install-build-deps.sh --no-syms --no-arm --no-chromeos-fonts --no-nacl
-#gclient runhooks && ninja -C out/Release quic_client quic_server net_unittests
+cd $third_party_dir/proto-quic
+export PATH=$PATH:`pwd`/depot_tools
+cd $third_party_dir/proto-quic/src
+./build/install-build-deps.sh --no-syms --no-arm --no-chromeos-fonts --no-nacl
+gclient runhooks && ninja -C out/Release quic_client quic_server net_unittests
--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -xe
 
-sudo apt-get -yq --force-yes install debian-archive-keyring
-sudo apt-key update
 sudo add-apt-repository -y ppa:keithw/mahimahi
 sudo apt-get -yq update
 sudo apt-get -yq --force-yes install mahimahi
