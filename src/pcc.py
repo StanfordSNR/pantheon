e1251a0b2c978886044195b9135174edaf302192
Francis <yuyan0@stanford.edu>
2016-05-03 20:28:00 -0700

    pcc.py: specify port to use and use check_call

--- src/pcc.py
+++ src/pcc.py
@@ -1,6 +1,12 @@
-import os, sys, subprocess
-from subprocess import Popen, PIPE
-from usage import general_usage
+#!/usr/bin/python
+
+import os, sys, time
+from subprocess import check_output, check_call, PIPE, Popen 
+import usage
+
+def print_usage():
+    usage.print_usage(os.path.basename(__file__))
+    sys.exit(1)
 
 def main():
     # find paths of this script, find_unused_port and scheme source to run
@@ -10,44 +16,45 @@ def main():
     pcc_dir = os.path.abspath(os.path.join(src_dir, '../third_party/pcc'))
     recv_file = os.path.join(pcc_dir, 'receiver/app/appserver')
     send_file = os.path.join(pcc_dir, 'sender/app/appclient')
+    DEVNULL = open(os.devnull, 'wb')
 
     if len(sys.argv) < 2:
-        general_usage()
-        return
+        print_usage()
 
     option = sys.argv[1]
 
     # setup
     if option == 'setup':
         if len(sys.argv) != 2: 
-            general_usage()
-            return
-        sys.stderr.write("Setup done.\n")
+            print_usage()
+
+        sys.stderr.write("Receiver first.\n")
 
     # receiver
     if option == 'receiver':
         if len(sys.argv) != 2: 
-            general_usage()
-            return
+            print_usage()
 
-        sys.stderr.write("Listening on port: 9000\n")
+        port = check_output([find_unused_port_file])
+        sys.stderr.write("Listening on port: %s\n" % port)
 
-        cmd = 'export LD_LIBRARY_PATH=%s && %s > /dev/null 2>&1' % \
-              (os.path.join(pcc_dir, 'receiver/src'), recv_file)
-        subprocess.call(cmd, shell=True)
+        cmd = 'export LD_LIBRARY_PATH=%s && %s %s' % \
+              (os.path.join(pcc_dir, 'receiver/src'), recv_file, port)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL, shell=True)
 
     # sender
     if option == 'sender':
         if len(sys.argv) != 4:
-            general_usage()
-            return
+            print_usage()
 
         ip = sys.argv[2]
         port = sys.argv[3] 
 
-        cmd = 'export LD_LIBRARY_PATH=%s && %s %s %s > /dev/null 2>&1' % \
+        cmd = 'export LD_LIBRARY_PATH=%s && %s %s %s' % \
               (os.path.join(pcc_dir, 'sender/src'), send_file, ip, port)
-        subprocess.call(cmd, shell=True)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL, shell=True)
+
+    DEVNULL.close()
     
 if __name__ == '__main__':
     main()
