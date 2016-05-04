466a5e669936260713224e7b340a7695616706f9
Francis <yuyan0@stanford.edu>
2016-05-03 20:43:28 -0700

    remove verus since it corrupts while running

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,5 @@ sudo apt-get update
 sudo apt-get -y --force-yes install mahimahi
 sudo sysctl -w net.ipv4.ip_forward=1
 
-sudo apt-get -y --force-yes install build-essential autoconf libtbb-dev \
-libasio-dev libalglib-dev libboost-system-dev python-pip iperf libnss3-tools
+sudo apt-get -y --force-yes install python-pip iperf libnss3-tools
 pip install interruptingcow
