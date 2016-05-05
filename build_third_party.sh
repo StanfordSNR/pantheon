cbca63d488673c3b481160161b46aab412997ab2
Francis <yuyan0@stanford.edu>
2016-05-04 19:41:45 -0700

    initialize certificate database

--- build_third_party.sh
+++ build_third_party.sh
@@ -17,5 +17,7 @@ make
 cd $third_party_dir/proto-quic
 export PATH=$PATH:`pwd`/depot_tools
 cd $third_party_dir/proto-quic/src
-# ./build/install-build-deps.sh --no-syms --no-arm --no-chromeos-fonts --no-nacl
 gclient runhooks && ninja -C out/Release quic_client quic_server net_unittests
+# initialize certificate database
+mkdir -p $HOME/.pki/nssdb
+certutil -d $HOME/.pki/nssdb -N
