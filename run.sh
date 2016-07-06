608b29221fd9211f1689cc726f064961e9994d56
Francis <francisyyan@stanford.edu>
2016-07-06 00:49:38 -0700

    Run tests in a separate shell script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -xe
+
+proj_dir=$( cd $(dirname $0) ; pwd )
+test_dir=$proj_dir/test
+
+cd $test_dir
+./test.py default_tcp
+./test.py vegas
+./test.py ledbat
+./test.py pcc
+./test.py verus 
+# - ./test.py quic
+./test.py scream
+./test.py sprout
