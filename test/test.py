ad734db6188d6924cb912256813a2d29ab9b276e
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:47:01 -0700

    Build in test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 import os, sys, signal
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_call
 import unittest
 
 # print test usage
 def test_usage():
-    print "Test Usage:"
-    print "./test.py congestion-control-name"
+    print 'Test Usage:'
+    print './test.py <congestion-control-name>'
     sys.exit(1)
 
 class TestCongestionControl(unittest.TestCase):
@@ -15,10 +15,6 @@ class TestCongestionControl(unittest.TestCase):
     def __init__(self, test_name, cc_option):
         super(TestCongestionControl, self).__init__(test_name)
         self.cc_option = cc_option
-        self.DEVNULL = open(os.devnull, 'wb')
-
-    def __del__(self):
-        self.DEVNULL.close()
 
     def timeout_handler(signum, frame):
         raise
@@ -29,24 +25,24 @@ class TestCongestionControl(unittest.TestCase):
             second_to_run = 'receiver'
 
         # run the side specified by first_to_run
-        cmd1 = ['python', self.src_file, first_to_run]
-        proc1 = Popen(cmd1, stdout=self.DEVNULL, stderr=PIPE)
+        cmd = 'python %s %s' % (self.src_file, first_to_run)
+        sys.stderr.write(cmd + '\n')
+        sys.stderr.write('Running %s %s...\n' % (self.cc_option, first_to_run))
+        proc1 = Popen(cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
 
         # find port printed
         port_info = proc1.stderr.readline()
         port = port_info.rstrip().rsplit(' ', 1)[-1]
 
         # run the other side specified by second_to_run
-        cmd2 = "'python %s %s %s %s'" % (self.src_file, second_to_run,
-                                         self.ip, port)
+        cmd = "'python %s %s %s %s'" % (self.src_file, second_to_run,
+                                        self.ip, port)
         mm_cmd = 'mm-link %s %s --once --uplink-log=%s --downlink-log=%s' \
                  ' -- sh -c %s' % (self.uplink_trace, self.downlink_trace,
-                 self.uplink_log, self.downlink_log, cmd2)
-
-        proc2 = Popen(mm_cmd, shell=True,
-                      stdout=self.DEVNULL, stderr=PIPE)
-
-        sys.stderr.write('Running %s...\n' % self.cc_option)
+                 self.uplink_log, self.downlink_log, cmd)
+        sys.stderr.write(mm_cmd + '\n')
+        sys.stderr.write('Running %s %s...\n' % (self.cc_option, second_to_run))
+        proc2 = Popen(mm_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
 
         # running for 60 seconds
         signal.signal(signal.SIGALRM, self.timeout_handler)
@@ -73,9 +69,18 @@ class TestCongestionControl(unittest.TestCase):
         src_dir = os.path.abspath(os.path.join(test_dir, '../src'))
         self.src_file = os.path.join(src_dir, cc_option + '.py')
 
+        # run build
+        build_cmd = 'python %s build' % self.src_file
+        sys.stderr.write(build_cmd + '\n')
+        sys.stderr.write('Building...\n')
+        check_call(build_cmd, stdout=DEVNULL, stderr=DEVNULL, shell=True)
+        sys.stderr.write('Done\n')
+
         # run setup
-        setup_cmd = ['python', self.src_file, 'setup']
-        setup_proc = Popen(setup_cmd, stdout=self.DEVNULL, stderr=PIPE)
+        setup_cmd = 'python %s setup' % self.src_file
+        sys.stderr.write(setup_cmd + '\n')
+        sys.stderr.write('Setting up...\n')
+        setup_proc = Popen(setup_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
         setup_info = setup_proc.communicate()[1]
         if setup_proc.returncode != 0:
             sys.stderr.write(setup_info)
@@ -84,13 +89,14 @@ class TestCongestionControl(unittest.TestCase):
         setup_info = setup_info.rstrip().rsplit('\n', 1)[-1]
         first_to_run = setup_info.split(' ')[0].lower()
         if first_to_run != 'receiver' and first_to_run != 'sender':
-            sys.stderr.write('Requires specifying receiver or sender first')
+            sys.stderr.write('Need to specify receiver or sender first\n')
             sys.exit(1)
+        sys.stderr.write('Done\n')
 
         # prepare mahimahi
         traces_dir = '/usr/share/mahimahi/traces/'
-        self.datalink_log = os.path.join(test_dir, '%s_datalink.log' % cc_option)
-        self.acklink_log = os.path.join(test_dir, '%s_acklink.log' % cc_option)
+        self.datalink_log = '%s/%s_datalink.log' % (test_dir, cc_option)
+        self.acklink_log = '%s/%s_acklink.log' % (test_dir, cc_option)
 
         if first_to_run == 'receiver':
             self.uplink_trace = traces_dir + 'Verizon-LTE-driving.up'
@@ -110,8 +116,8 @@ class TestCongestionControl(unittest.TestCase):
 
         # generate throughput graphs
         sys.stderr.write('* Data link statistics:\n')
-        datalink_throughput = open(os.path.join(test_dir,
-                                '%s_datalink_throughput.html' % cc_option), 'wb')
+        datalink_throughput = open('%s/%s_datalink_throughput.html' \
+                                   % (test_dir, cc_option), 'wb')
         proc = Popen(['mm-throughput-graph', '500', self.datalink_log],
                      stdout=datalink_throughput, stderr=PIPE)
         datalink_results = proc.communicate()[1]
@@ -119,8 +125,8 @@ class TestCongestionControl(unittest.TestCase):
         datalink_throughput.close()
 
         sys.stderr.write('* ACK link statistics:\n')
-        acklink_throughput = open(os.path.join(test_dir,
-                                '%s_acklink_throughput.html' % cc_option), 'wb')
+        acklink_throughput = open('%s/%s_acklink_throughput.html' \
+                                  % (test_dir, cc_option), 'wb')
         proc = Popen(['mm-throughput-graph', '500', self.acklink_log],
                      stdout=acklink_throughput, stderr=PIPE)
         acklink_results = proc.communicate()[1]
@@ -130,15 +136,19 @@ class TestCongestionControl(unittest.TestCase):
 def main():
     if len(sys.argv) != 2:
         test_usage()
-
     cc_option = sys.argv[1]
+
+    # Enable IP forwarding
+    cmd = ['sudo', 'sysctl', '-w', 'net.ipv4.ip_forward=1']
+    check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     # create test suite to run
     suite = unittest.TestSuite()
-    suite.addTest(TestCongestionControl("test_congestion_control", cc_option))
-    success = unittest.TextTestRunner().run(suite).wasSuccessful()
-    if not success:
+    suite.addTest(TestCongestionControl('test_congestion_control', cc_option))
+    if not unittest.TextTestRunner().run(suite).wasSuccessful():
         sys.exit(1)
 
 if __name__ == '__main__':
-   main()
+    DEVNULL = open(os.devnull, 'wb')
+    main()
+    DEVNULL.close()
