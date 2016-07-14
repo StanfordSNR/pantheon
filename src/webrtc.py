4357387055f124c4c766f359d9816b2507ac8194
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:43:59 -0700

    Add build option to webrtc.py

--- src/webrtc.py
+++ src/webrtc.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python
 
 import os, sys, time
-from subprocess import check_call
+from subprocess import check_call, Popen
 import usage
 
-def print_usage():
-    usage.print_usage(os.path.basename(__file__), order=usage.SEND_FIRST)
-    sys.exit(1)
-
 def main():
-    # find paths of this script, find_unused_port and scheme source to run
+    usage.check_args(sys.argv, os.path.basename(__file__), usage.SEND_FIRST)
+    option = sys.argv[1]
     src_dir = os.path.abspath(os.path.dirname(__file__))
-    src_file = os.path.abspath(os.path.join(src_dir,
-                               '../third_party/webrtc/app.js'))
+    submodule_dir = os.path.abspath(os.path.join(src_dir,
+                                    '../third_party/webrtc'))
+    src_file = os.path.join(submodule_dir, 'app.js')
     video_file = os.path.abspath('/tmp/video.y4m')
     DEVNULL = open(os.devnull, 'wb')
 
-    if len(sys.argv) < 2:
-        print_usage()
-
-    option = sys.argv[1]
+    # build
+    if option == 'build':
+        cmd = 'cd %s && npm install' % submodule_dir
+        check_call(cmd, shell=True)
 
     # setup
     if option == 'setup':
-        if len(sys.argv) != 2:
-            print_usage()
-
+        video_url = 'http://media.xiph.org/video/derf/y4m/city_cif_15fps.y4m'
+        cmd = ['wget', '-O', video_file, video_url]
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
         sys.stderr.write("Sender first\n")
 
     # sender
     if option == 'sender':
-        if len(sys.argv) != 2:
-            print_usage()
-
+        cmd = ['Xvfb', ':1']
+        xvfb = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL)
+        os.environ['DISPLAY']=':1'
+        cmd = ['node', src_file]
+        signaling_server = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL)
         sys.stderr.write("Listening on port: %s\n" % 3000)
-
         cmd = 'chromium-browser --app=http://localhost:3000/sender ' \
               '--use-fake-ui-for-media-stream ' \
               '--use-fake-device-for-media-stream ' \
@@ -44,12 +43,11 @@ def main():
 
     # receiver
     if option == 'receiver':
-        if len(sys.argv) != 4:
-            print_usage()
-
+        cmd = ['Xvfb', ':2']
+        xvfb = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL)
+        os.environ['DISPLAY']=':2'
         ip = sys.argv[2]
         port = sys.argv[3]
-
         time.sleep(3) # at least wait until the sender is ready
         cmd = 'chromium-browser --app=http://%s:%s/receiver ' \
               '--user-data-dir=/tmp/nonexistent$(date +%%s%%N)' % (ip, port)
