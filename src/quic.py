8b18e7f70df572a626842572eceb2fbeab4fd165
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:43:31 -0700

    Add build option to quic.py

--- src/quic.py
+++ src/quic.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python
 
-import os, sys, time, errno
-from subprocess import check_output, check_call, PIPE, Popen
+import os, sys, errno
+from subprocess import check_call
 import usage
 from generate_html import generate_html
-
-def print_usage():
-    usage.print_usage(os.path.basename(__file__), order=usage.SEND_FIRST)
-    sys.exit(1)
 
 def setup():
     # generate a random password
@@ -41,54 +37,46 @@ def setup():
     generate_html(50000000)
 
 def main():
-    # find paths of this script, find_unused_port and scheme source to run
-    src_dir = os.path.abspath(os.path.dirname(__file__))
-    find_unused_port_file = os.path.join(src_dir, 'find_unused_port')
-    quic_src_dir = os.path.abspath(os.path.join(src_dir,
-                                    '../third_party/proto-quic/src'))
-
-    quic_server = os.path.join(quic_src_dir, 'out/Release/quic_server')
-    quic_client = os.path.join(quic_src_dir, 'out/Release/quic_client')
-
-    if len(sys.argv) < 2:
-        print_usage()
-
+    usage.check_args(sys.argv, os.path.basename(__file__), usage.SEND_FIRST)
     option = sys.argv[1]
+    src_dir = os.path.abspath(os.path.dirname(__file__))
+    submodule_dir = os.path.abspath(os.path.join(src_dir,
+                                    '../third_party/proto-quic'))
+    find_unused_port_file = os.path.join(src_dir, 'find_unused_port')
+    quic_server = os.path.join(submodule_dir, 'src/out/Release/quic_server')
+    quic_client = os.path.join(submodule_dir, 'src/out/Release/quic_client')
+    DEVNULL = open(os.devnull, 'wb')
+
+    # build
+    if option == 'build':
+        os.environ['PATH'] += ':%s/depot_tools' % submodule_dir
+        cmd = 'cd %s/src && gclient runhooks && ninja -C out/Release ' \
+              'quic_client quic_server' % submodule_dir
+        check_call(cmd, shell=True)
 
     # setup
     if option == 'setup':
-        if len(sys.argv) != 2:
-            print_usage()
-
         setup()
         sys.stderr.write("Sender first\n")
 
     # sender
     if option == 'sender':
-        if len(sys.argv) != 2:
-            print_usage()
-
         sys.stderr.write("Listening on port: 6121\n")
-
         cmd = [quic_server,
               '--quic_in_memory_cache_dir=/tmp/quic-data/www.example.org',
-              '--certificate_file=%s' % \
-                 os.path.join(src_dir, 'certs/out/leaf_cert.pem'),
-              '--key_file=%s' % \
-                 os.path.join(src_dir, 'certs/out/leaf_cert.pkcs8')]
-        check_call(cmd)
+              '--certificate_file=%s/certs/out/leaf_cert.pem' % src_dir,
+              '--key_file=%s/certs/out/leaf_cert.pkcs8' % src_dir]
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     # receiver
     if option == 'receiver':
-        if len(sys.argv) != 4:
-            print_usage()
-
         ip = sys.argv[2]
         port = sys.argv[3]
-
         cmd = [quic_client, '--host=%s' % ip, '--port=%s' % port,
               'https://www.example.org/']
-        check_call(cmd)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
+
+    DEVNULL.close()
 
 if __name__ == '__main__':
     main()
