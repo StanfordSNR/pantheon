011ef876b46ad9c13e27b18648c8ad402ba6ed9e
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:44:14 -0700

    Add build option to sprout.py

--- src/sprout.py
+++ src/sprout.py
@@ -1,52 +1,46 @@
 #!/usr/bin/python
 
 import os, sys
-from subprocess import check_output, check_call, PIPE
+from subprocess import check_call
 import usage
 
-def print_usage():
-    usage.print_usage(os.path.basename(__file__))
-    sys.exit(1)
-
 def main():
-    # find paths of this script, find_unused_port and scheme source to run
+    usage.check_args(sys.argv, os.path.basename(__file__), usage.RECV_FIRST)
+    option = sys.argv[1]
     src_dir = os.path.abspath(os.path.dirname(__file__))
-    src_file = os.path.abspath(os.path.join(src_dir,
-                               '../third_party/sprout/src/examples/sproutbt2'))
+    submodule_dir = os.path.abspath(os.path.join(src_dir,
+                                    '../third_party/sprout'))
+    src_file = os.path.join(submodule_dir, 'src/examples/sproutbt2')
     DEVNULL = open(os.devnull, 'wb')
 
-    if len(sys.argv) < 2:
-        print_usage()
-
-    option = sys.argv[1]
+    # build
+    if option == 'build':
+        cmd = 'cd %s && ./autogen.sh && ./configure --enable-examples && ' \
+              'make -j' % submodule_dir
+        check_call(cmd, shell=True)
 
     # setup
     if option == 'setup':
-        if len(sys.argv) != 2:
-            print_usage()
-
         sys.stderr.write("Receiver first\n")
 
     # receiver
     if option == 'receiver':
-        if len(sys.argv) != 2:
-            print_usage()
-
+        os.environ['SPROUT_MODEL_IN'] = '%s/src/examples/sprout.model' \
+                                        % submodule_dir
+        print os.environ['SPROUT_MODEL_IN']
         sys.stderr.write("Listening on port: %s\n" % 60001)
-
         cmd = [src_file]
-        check_call(cmd)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     # sender
     if option == 'sender':
-        if len(sys.argv) != 4:
-            print_usage()
-
+        os.environ['SPROUT_MODEL_IN'] = '%s/src/examples/sprout.model' \
+                                        % submodule_dir
+        print os.environ['SPROUT_MODEL_IN']
         ip = sys.argv[2]
         port = sys.argv[3]
-
         cmd = [src_file, ip, port]
-        check_call(cmd)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     DEVNULL.close()
 
