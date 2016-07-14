73d14ebb3cf6e8f3a0cabad0fa2388c470bdf008
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:42:59 -0700

    Move args check to usage.py

--- src/usage.py
+++ src/usage.py
@@ -1,8 +1,11 @@
+import sys
+
 RECV_FIRST = 0
 SEND_FIRST = 1
 
-def print_usage(name, order=RECV_FIRST):
+def print_usage(name, order):
     print "Usage:"
+    print "./%s build" % name
     print "./%s setup" % name
     if order == RECV_FIRST:
         print "./%s receiver" % name
@@ -10,3 +13,32 @@ def print_usage(name, order=RECV_FIRST):
     elif order == SEND_FIRST:
         print "./%s sender" % name
         print "./%s receiver IP port" % name
+    sys.exit(1)
+
+def check_args(args, name, order):
+    if len(args) < 2:
+        print_usage(name, order)
+
+    option = args[1]
+
+    if option == 'build' or option == 'setup':
+        if len(args) != 2:
+            print_usage(name, order)
+
+    if option == 'receiver':
+        if order == RECV_FIRST and len(args) != 2:
+            print_usage(name, order)
+
+        if order == SEND_FIRST and len(args) != 4:
+            print_usage(name, order)
+
+    if option == 'sender':
+        if order == RECV_FIRST and len(args) != 4:
+            print_usage(name, order)
+
+        if order == SEND_FIRST and len(args) != 2:
+            print_usage(name, order)
+
+    if option != 'build' and option != 'setup' and \
+       option != 'receiver' and option != 'sender':
+        print_usage(name, order)
