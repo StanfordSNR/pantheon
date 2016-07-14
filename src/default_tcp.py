a6f9dc27a5f37ba1caa8422bd9fff743d2af3217
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:43:12 -0700

    Add build option to default_tcp.py

--- src/default_tcp.py
+++ src/default_tcp.py
@@ -4,48 +4,37 @@ import os, sys
 from subprocess import check_output, check_call
 import usage
 
-def print_usage():
-    usage.print_usage(os.path.basename(__file__))
-    sys.exit(1)
-
 def main():
+    usage.check_args(sys.argv, os.path.basename(__file__), usage.RECV_FIRST)
+    option = sys.argv[1]
     src_dir = os.path.abspath(os.path.dirname(__file__))
     find_unused_port_file = os.path.join(src_dir, 'find_unused_port')
     src_file = 'iperf'
+    DEVNULL = open(os.devnull, 'wb')
 
-    if len(sys.argv) < 2:
-        print_usage()
-
-    option = sys.argv[1]
+    # build
+    if option == 'build':
+        pass
 
     # setup
     if option == 'setup':
-        if len(sys.argv) != 2:
-            print_usage()
-
-        sys.stderr.write("Receiver first\n")
+        sys.stderr.write('Receiver first\n')
 
     # receiver
     if option == 'receiver':
-        if len(sys.argv) != 2:
-            print_usage()
-
         port = check_output([find_unused_port_file])
-        sys.stderr.write("Listening on port: %s\n" % port)
-
+        sys.stderr.write('Listening on port: %s\n' % port)
         cmd = [src_file, '-s', '-p', port]
-        check_call(cmd)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     # sender
     if option == 'sender':
-        if len(sys.argv) != 4:
-            print_usage()
-
         ip = sys.argv[2]
         port = sys.argv[3]
-
         cmd = [src_file, '-c', ip, '-p', port, '-t', '75']
-        check_call(cmd)
+        check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
+
+    DEVNULL.close()
 
 if __name__ == '__main__':
     main()
