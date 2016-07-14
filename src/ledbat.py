9d0e4b170bc28d132d2aab4bb950ac9faa7e0404
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-13 23:43:19 -0700

    Add build option to ledbat.py

--- src/ledbat.py
+++ src/ledbat.py
@@ -4,51 +4,38 @@ import os, sys, time
 from subprocess import check_output, check_call, PIPE, Popen
 import usage
 
-def print_usage():
-    usage.print_usage(os.path.basename(__file__))
-    sys.exit(1)
-
 def main():
-    # find paths of this script, find_unused_port and scheme source to run
+    usage.check_args(sys.argv, os.path.basename(__file__), usage.RECV_FIRST)
+    option = sys.argv[1]
     src_dir = os.path.abspath(os.path.dirname(__file__))
+    submodule_dir = os.path.abspath(os.path.join(src_dir,
+                                    '../third_party/libutp'))
     find_unused_port_file = os.path.join(src_dir, 'find_unused_port')
-    src_file = os.path.abspath(os.path.join(src_dir,
-                               '../third_party/libutp/ucat-static'))
+    src_file = os.path.join(submodule_dir, 'ucat-static')
     DEVNULL = open(os.devnull, 'wb')
 
-    if len(sys.argv) < 2:
-        print_usage()
-
-    option = sys.argv[1]
+    # build
+    if option == 'build':
+        cmd = 'cd %s && make -j' % submodule_dir
+        check_call(cmd, shell=True)
 
     # setup
     if option == 'setup':
-        if len(sys.argv) != 2:
-            print_usage()
-
-        sys.stderr.write("Receiver first\n")
+        sys.stderr.write('Receiver first\n')
 
     # receiver
     if option == 'receiver':
-        if len(sys.argv) != 2:
-            print_usage()
-
         port = check_output([find_unused_port_file])
-        sys.stderr.write("Listening on port: %s\n" % port)
-
+        sys.stderr.write('Listening on port: %s\n' % port)
         cmd = [src_file, '-l', '-p', port]
         check_call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
     # sender
     if option == 'sender':
-        if len(sys.argv) != 4:
-            print_usage()
-
         ip = sys.argv[2]
         port = sys.argv[3]
-
         cmd = [src_file, ip, port]
-        proc = Popen(cmd, stdin=PIPE)
+        proc = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE)
 
         timeout = time.time() + 75
         while True:
