4a66c931a663273455dc409ff53510df31849830
Francis <francisyyan@stanford.edu>
2016-07-17 11:36:44 -0700

    install_deps.sh: add comments for global dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -xe
 
-# mahimahi
+# Install mahimahi
 sudo add-apt-repository -y ppa:keithw/mahimahi
 sudo apt-get -yq update
 sudo apt-get -yq --force-yes install mahimahi
+
+# Add NodeSource PPA (to access the latest nodejs)
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
