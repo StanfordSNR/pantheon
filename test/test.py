d7ec6d9e4e60596c96a82723deb09e2182be18eb
Francis <francisyyan@stanford.edu>
2016-07-17 01:30:03 -0700

    test.py: separate different phases as function calls

--- test/test.py
+++ test/test.py
@@ -14,6 +14,65 @@ class TestCongestionControl(unittest.TestCase):
     def timeout_handler(signum, frame):
         raise
 
+    def install(self):
+        deps_cmd = 'python %s deps' % self.src_file
+        sys.stderr.write('+ ' + deps_cmd + '\n')
+        deps_proc = Popen(deps_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
+        deps_needed = deps_proc.communicate()[1]
+        if deps_proc.returncode != 0:
+            sys.stderr.write(deps_needed)
+            sys.exit(1)
+
+        if deps_needed:
+            sys.stderr.write('Installing dependencies...\n')
+            sys.stderr.write(deps_needed)
+            install_cmd = 'sudo apt-get -yq --force-yes install ' + deps_needed
+            check_call(install_cmd , shell=True)
+        sys.stderr.write('Done\n')
+
+    def build(self):
+        build_cmd = 'python %s build' % self.src_file
+        sys.stderr.write('+ ' + build_cmd + '\n')
+        sys.stderr.write('Building...\n')
+        check_call(build_cmd, shell=True)
+        sys.stderr.write('Done\n')
+
+    def setup(self):
+        setup_cmd = 'python %s setup' % self.src_file
+        sys.stderr.write('+ ' + setup_cmd + '\n')
+        sys.stderr.write('Setting up...\n')
+        setup_proc = Popen(setup_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
+        setup_info = setup_proc.communicate()[1]
+        if setup_proc.returncode != 0:
+            sys.stderr.write(setup_info)
+            sys.exit(1)
+
+        setup_info = setup_info.rstrip().rsplit('\n', 1)[-1]
+        self.first_to_run = setup_info.split(' ')[0].lower()
+        if self.first_to_run != 'receiver' and self.first_to_run != 'sender':
+            sys.stderr.write('Need to specify receiver or sender first\n')
+            sys.exit(1)
+        sys.stderr.write('Done\n')
+
+    def prepare_mahimahi(self):
+        self.ip = '$MAHIMAHI_BASE'
+        traces_dir = '/usr/share/mahimahi/traces/'
+        self.datalink_log = '%s/%s_datalink.log' % (self.test_dir, self.cc_option)
+        self.acklink_log = '%s/%s_acklink.log' % (self.test_dir, self.cc_option)
+
+        if self.first_to_run == 'receiver':
+            self.uplink_trace = traces_dir + 'Verizon-LTE-driving.up'
+            self.downlink_trace = traces_dir + 'Verizon-LTE-driving.down'
+            self.uplink_log = self.datalink_log
+            self.downlink_log = self.acklink_log
+            self.second_to_run = 'sender'
+        else:
+            self.uplink_trace = traces_dir + 'Verizon-LTE-driving.down'
+            self.downlink_trace = traces_dir + 'Verizon-LTE-driving.up'
+            self.uplink_log = self.acklink_log
+            self.downlink_log = self.datalink_log
+            self.second_to_run = 'receiver'
+
     def run_congestion_control(self):
         # run the side specified by self.first_to_run
         cmd = 'python %s %s' % (self.src_file, self.first_to_run)
@@ -38,7 +97,7 @@ class TestCongestionControl(unittest.TestCase):
         sys.stderr.write('Running %s %s...\n' % (self.cc_option, self.second_to_run))
         proc2 = Popen(mm_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
 
-        # running for 60 seconds
+        # run for 60 seconds
         signal.signal(signal.SIGALRM, self.timeout_handler)
         signal.alarm(60)
 
@@ -53,79 +112,9 @@ class TestCongestionControl(unittest.TestCase):
         proc2.terminate()
         proc1.terminate()
 
-    # congestion control test
-    def test_congestion_control(self):
-        self.cc_option = sys.argv[1].lower()
-        self.test_dir = os.path.abspath(os.path.dirname(__file__))
-        src_dir = os.path.abspath(os.path.join(self.test_dir, '../src'))
-        self.src_file = os.path.join(src_dir, self.cc_option + '.py')
-
-        # get build dependencies
-        deps_cmd = 'python %s deps' % self.src_file
-        sys.stderr.write('+ ' + deps_cmd + '\n')
-        deps_proc = Popen(deps_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
-        deps_needed = deps_proc.communicate()[1]
-        if deps_proc.returncode != 0:
-            sys.stderr.write(deps_needed)
-            sys.exit(1)
-
-        if deps_needed:
-            sys.stderr.write('Installing dependencies...\n')
-            sys.stderr.write(deps_needed)
-            install_cmd = 'sudo apt-get -yq --force-yes install ' + deps_needed
-            check_call(install_cmd , shell=True)
-        sys.stderr.write('Done\n')
-
-        # run build
-        build_cmd = 'python %s build' % self.src_file
-        sys.stderr.write('+ ' + build_cmd + '\n')
-        sys.stderr.write('Building...\n')
-        check_call(build_cmd, shell=True)
-        sys.stderr.write('Done\n')
-
-        # run setup
-        setup_cmd = 'python %s setup' % self.src_file
-        sys.stderr.write('+ ' + setup_cmd + '\n')
-        sys.stderr.write('Setting up...\n')
-        setup_proc = Popen(setup_cmd, stdout=DEVNULL, stderr=PIPE, shell=True)
-        setup_info = setup_proc.communicate()[1]
-        if setup_proc.returncode != 0:
-            sys.stderr.write(setup_info)
-            sys.exit(1)
-
-        setup_info = setup_info.rstrip().rsplit('\n', 1)[-1]
-        self.first_to_run = setup_info.split(' ')[0].lower()
-        if self.first_to_run != 'receiver' and self.first_to_run != 'sender':
-            sys.stderr.write('Need to specify receiver or sender first\n')
-            sys.exit(1)
-        sys.stderr.write('Done\n')
-
-        # prepare mahimahi
-        traces_dir = '/usr/share/mahimahi/traces/'
-        self.datalink_log = '%s/%s_datalink.log' % (self.test_dir, self.cc_option)
-        self.acklink_log = '%s/%s_acklink.log' % (self.test_dir, self.cc_option)
-
-        if self.first_to_run == 'receiver':
-            self.uplink_trace = traces_dir + 'Verizon-LTE-driving.up'
-            self.downlink_trace = traces_dir + 'Verizon-LTE-driving.down'
-            self.uplink_log = self.datalink_log
-            self.downlink_log = self.acklink_log
-            self.second_to_run = 'sender'
-        else:
-            self.uplink_trace = traces_dir + 'Verizon-LTE-driving.down'
-            self.downlink_trace = traces_dir + 'Verizon-LTE-driving.up'
-            self.uplink_log = self.acklink_log
-            self.downlink_log = self.datalink_log
-            self.second_to_run = 'receiver'
-
-        self.ip = '$MAHIMAHI_BASE'
-
-        # run receiver and sender
-        self.run_congestion_control()
-
-        # generate throughput graphs
+    def gen_results(self):
         sys.stderr.write('* Data link statistics:\n')
-        datalink_throughput = open('%s/%s_datalink_throughput.html' \
+        datalink_throughput = open('%s/%s_datalink_throughput.svg' \
                                    % (self.test_dir, self.cc_option), 'wb')
         proc = Popen(['mm-throughput-graph', '500', self.datalink_log],
                      stdout=datalink_throughput, stderr=PIPE)
@@ -134,13 +123,38 @@ class TestCongestionControl(unittest.TestCase):
         datalink_throughput.close()
 
         sys.stderr.write('* ACK link statistics:\n')
-        acklink_throughput = open('%s/%s_acklink_throughput.html' \
+        acklink_throughput = open('%s/%s_acklink_throughput.svg' \
                                   % (self.test_dir, self.cc_option), 'wb')
         proc = Popen(['mm-throughput-graph', '500', self.acklink_log],
                      stdout=acklink_throughput, stderr=PIPE)
         acklink_results = proc.communicate()[1]
         sys.stderr.write(acklink_results)
         acklink_throughput.close()
+
+    # congestion control test
+    def test_congestion_control(self):
+        self.cc_option = sys.argv[1].lower()
+        self.test_dir = os.path.abspath(os.path.dirname(__file__))
+        src_dir = os.path.abspath(os.path.join(self.test_dir, '../src'))
+        self.src_file = os.path.join(src_dir, self.cc_option + '.py')
+
+        # get build dependencies
+        self.install()
+
+        # run build
+        self.build()
+
+        # run setup
+        self.setup()
+
+        # prepare mahimahi
+        self.prepare_mahimahi()
+
+        # run receiver and sender
+        self.run_congestion_control()
+
+        # generate results, including statistics and graphs
+        self.gen_results()
 
 def main():
     if len(sys.argv) != 2:
