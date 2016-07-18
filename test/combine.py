3446cadde2ece3a694ad6876d26e329b2e345869
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-17 22:37:01 -0700

    combine.py: combine PNGs and generate pdf using LaTeX

---
+++ test/combine.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import os, sys, string
+from subprocess import check_call
+
+def main():
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    latex = '\\documentclass{article}\n' \
+            '\\usepackage{graphicx}\n' \
+            '\\usepackage{float}\n' \
+            '\\begin{document}\n'
+
+    for i in range(1, len(sys.argv)):
+        cc = sys.argv[i]
+        latex += 'Congestion Control Report of %s --- Data Link\n\n' \
+                  % string.replace(cc, '_', '\_')
+
+        latex += '\\begin{figure}[H]\n\\centering\n'
+        latex += '\\includegraphics[width=\\textwidth]' \
+                 '{%s_datalink_throughput.png}\n' % cc
+        latex += '\\end{figure}\n\n'
+
+        latex += '\\begin{figure}[H]\n\\centering\n'
+        latex += '\\includegraphics[width=\\textwidth]' \
+                 '{%s_datalink_delay.png}\n' % cc
+        latex += '\\end{figure}\n\n'
+
+        latex += '\\newpage\n\n'
+
+        latex += 'Congestion Control Report of %s --- ACK Link\n\n' \
+                  % string.replace(cc, '_', '\_')
+
+        latex += '\\begin{figure}[H]\n\\centering\n'
+        latex += '\\includegraphics[width=\\textwidth]' \
+                 '{%s_acklink_throughput.png}\n' % cc
+        latex += '\\end{figure}\n\n'
+
+        latex += '\\begin{figure}[H]\n\\centering\n'
+        latex += '\\includegraphics[width=\\textwidth]' \
+                 '{%s_acklink_delay.png}\n' % cc
+        latex += '\\end{figure}\n\n'
+
+        if i < len(sys.argv) - 1:
+            latex += '\\newpage\n\n'
+
+    latex += '\\end{document}'
+
+    latex_file = open('pantheon-report.tex', 'wb')
+    latex_file.write(latex)
+    latex_file.close()
+
+    cmd = 'pdflatex pantheon-report.tex'
+    check_call(cmd, shell=True)
+
+if __name__ == '__main__':
+    main()
