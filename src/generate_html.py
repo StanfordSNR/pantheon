065ed968f7e07f100f39021cf9f79760fed8b36f
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-19 01:13:38 +0000

    generate random html write 1024 chars at a time

--- src/generate_html.py
+++ src/generate_html.py
@@ -12,22 +12,35 @@ def generate_html(size):
         if exception.errno != errno.EEXIST:
             raise
 
+    head_text = "HTTP/1.1 200 OK\n"\
+            "X-Original-Url: https://www.example.org/\n"\
+            "\n"\
+            "<!DOCTYPE html>\n"\
+            "<html>\n"\
+            "<body>\n"\
+            "<p>\n"
+
+    foot_text = "</p>\n"\
+            "</body>\n"\
+            "</html>\n"
+
+    i = len(head_text) + len(foot_text)
+    # check we can actually write something
+    assert( size >= i )
+
     f = open(file_name, 'wb')
-    f.write("HTTP/1.1 200 OK\n"
-            "X-Original-Url: https://www.example.org/\n"
-            "\n"
-            "<!DOCTYPE html>\n"
-            "<html>\n"
-            "<body>\n"
-            "<p>\n")
+    f.write(head_text)
 
-    i = 0
-    while i < size - 113: # the other parts are 113 bytes
-        f.write(random.choice(string.letters))
-        i += 1
+    block_size = 1024
+    while i < size:
+        if i + block_size <= size:
+            block = ''.join(random.choice(string.letters) for _ in range(block_size))
+            f.write(block)
+            i += block_size
+        else:
+            f.write(random.choice(string.letters))
+            i += 1
 
-    f.write("</p>\n"
-            "</body>\n"
-            "</html>\n")
+    f.write(foot_text)
 
     f.close()
