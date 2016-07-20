61bae441abc95e28c58105ae8ffc7e53553f9610
Francis Y. Yan <francisyyan@stanford.edu>
2016-07-20 01:53:33 -0700

    Move scream wrapper from a separate git repo to be part of Pantheon

---
+++ src/scream/Timerfd.hh
@@ -0,0 +1,27 @@
+#ifndef TIMERFD_H
+#define TIMERFD_H
+
+#include <stdint.h>
+#include <sys/timerfd.h>
+#include "file_descriptor.hh"
+
+class Timerfd : public FileDescriptor
+{
+public:
+  /* 
+   * Default is blocking timer 
+   * Pass in TFD_NONBLOCK for non-blocking timer 
+   */
+  Timerfd(int flags = 0);
+
+  /* Start the timer with first expiration time and period */
+  void arm(int first_exp_ms, int interval_ms = 0);
+
+  /* Return true if the timer has stopped */
+  bool is_disarmed();
+
+  /* Return the number of expirations occurred */
+  int expirations();
+};
+
+#endif /* TIMERFD_H */
