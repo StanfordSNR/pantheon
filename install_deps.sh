a658d94d70c915af18289f94645dabda3146a078
Keith Winstein <keithw@cs.stanford.edu>
2016-07-21 12:45:50 +0200

    Move most dependencies into .travis.yml

--- install_deps.sh
+++ install_deps.sh
@@ -1,13 +1,4 @@
 #!/bin/sh -xe
 
-# Install mahimahi and inkscape
-sudo add-apt-repository -y ppa:keithw/mahimahi
-sudo add-apt-repository -y ppa:inkscape.dev/stable
-sudo apt-get -yq update
-sudo apt-get -yq --force-yes install mahimahi inkscape
-
 # Add NodeSource PPA (to access the latest nodejs)
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-
-# Install TeX Live
-sudo apt-get install -yq --force-yes texlive
