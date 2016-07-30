e7c761f16d421442f22a6cf8d6d84b495ac39867
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-30 00:17:46 +0000

    rename test.py to test_locally.py

--- run.sh
+++ run.sh
@@ -5,32 +5,32 @@ test_dir=$proj_dir/test
 
 cd $test_dir
 ./setup.py default_tcp
-./test.py  default_tcp
+./test_locally.py  default_tcp
 
 ./setup.py vegas
-./test.py  vegas
+./test_locally.py  vegas
 
 ./setup.py ledbat
-./test.py  ledbat
+./test_locally.py  ledbat
 
 ./setup.py pcc
-./test.py  pcc
+./test_locally.py  pcc
 
 ./setup.py verus
-./test.py  verus
+./test_locally.py  verus
 
 ./setup.py scream
-./test.py  scream
+./test_locally.py  scream
 
 ./setup.py webrtc
-./test.py  webrtc
+./test_locally.py  webrtc
 
 ./setup.py sprout
-./test.py  sprout
+./test_locally.py  sprout
 
 # run quic last as it takes the longest by far
 ./setup.py quic
-./test.py  quic
+./test_locally.py  quic
 
 # Assemble a throughput-delay plot
 ./summary-plot.pl pantheon_summary.pdf default_tcp vegas ledbat pcc verus scream webrtc sprout quic
