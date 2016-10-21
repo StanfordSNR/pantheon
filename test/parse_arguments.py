3f58419a34d63448c9317b5b2d384959c061ebbe
Francis Y. Yan <francisyyan@stanford.edu>
2016-10-20 21:45:39 -0700

    parse_arguments.py: refactor arguments parsers

--- test/parse_arguments.py
+++ test/parse_arguments.py
@@ -5,21 +5,25 @@ import sys
 def parse_arguments(filename):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-r', action='store', dest='remote', type=str,
-                        help='remote pantheon directory: [user@]hostname:dir')
     parser.add_argument('-i', action='store', dest='private_key', type=str,
                         help='identity file (private key) for ssh/scp to use')
+    parser.add_argument('-r', action='store', dest='remote', type=str,
+                        help='remote pantheon directory: [user@]hostname:dir')
 
-    if filename != 'setup.py':
+    if filename == 'run.py' or filename == 'test.py':
         parser.add_argument('-f', action='store', dest='flows', type=int,
                             default=1, help='number of flows '
                             '(mm-tunnelclient/mm-tunnelserver pairs)')
         parser.add_argument('-t', action='store', dest='runtime', type=int,
                             default=60, help='running time of each test')
 
-    if filename != 'run.py':
+    if filename == 'test.py':
         parser.add_argument('cc', metavar='congestion-control', type=str,
                             help='name of a congestion control scheme')
+    elif filename == 'setup.py':
+        parser.add_argument(
+            'cc', metavar='mahimahi|congestion-control', type=str,
+            help='setup mahimahi before setup any congestion control')
 
     args = parser.parse_args()
 
