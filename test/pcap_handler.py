aa6bb46c149643334cdf7d11fe260d8c2f67c8ab
Francis Y. Yan <francisyyan@stanford.edu>
2016-10-26 22:37:33 -0700

    pcap_handler.py: abandon acklink results due to indistinguishable same uid

--- test/pcap_handler.py
+++ test/pcap_handler.py
@@ -56,39 +56,26 @@ def main():
     sys.stderr.write('+ ' + ' '.join(convert_cmd) + '\n')
     check_call(convert_cmd)
 
-    # generate "raw" (without tunnel) datalink and acklink log
+    # generate "raw" (without tunnel) datalink log
     datalink_log = path.join(test_dir, args.cc + '_raw_datalink.log')
-    acklink_log = path.join(test_dir, args.cc + '_raw_acklink.log')
 
     if first_to_run == 'receiver':
         datalink_cmd = ['mm-tunnel-merge-logs', 'single', '-i', server_ilog,
                         '-e', client_elog, '-o', datalink_log]
-        acklink_cmd = ['mm-tunnel-merge-logs', 'single', '-i', client_ilog,
-                       '-e', server_elog, '-o', acklink_log]
     else:
         datalink_cmd = ['mm-tunnel-merge-logs', 'single', '-i', client_ilog,
                         '-e', server_elog, '-o', datalink_log]
-        acklink_cmd = ['mm-tunnel-merge-logs', 'single', '-i', server_ilog,
-                       '-e', client_elog, '-o', acklink_log]
 
     sys.stderr.write('+ ' + ' '.join(datalink_cmd) + '\n')
     check_call(datalink_cmd)
 
-    sys.stderr.write('+ ' + ' '.join(acklink_cmd) + '\n')
-    check_call(acklink_cmd)
-
     # generate statistical results
     throughput_cmd = 'mm-tunnel-throughput'
-    delay_cmd = 'mm-tunnel-delay'
 
     datalink_throughput_svg = path.join(
         test_dir, '%s_raw_data_tput.svg' % args.cc)
     datalink_delay_svg = path.join(
         test_dir, '%s_raw_data_delay.svg' % args.cc)
-    acklink_throughput_svg = path.join(
-        test_dir, '%s_raw_ack_tput.svg' % args.cc)
-    acklink_delay_svg = path.join(
-        test_dir, '%s_raw_ack_delay.svg' % args.cc)
 
     stats_log = path.join(test_dir, '%s_raw_stats.log' % args.cc)
     stats = open(stats_log, 'w')
@@ -119,33 +106,6 @@ def main():
     proc.communicate()
 
     datalink_delay.close()
-
-    sys.stderr.write('\n')
-    # ACK link
-    # throughput
-    acklink_throughput = open(acklink_throughput_svg, 'w')
-    cmd = [throughput_cmd, '500', acklink_log]
-
-    sys.stderr.write('+ ' + ' '.join(cmd) + '\n')
-    sys.stderr.write('* ACK link statistics:\n')
-    stats.write('* ACK link statistics:\n')
-
-    proc = Popen(cmd, stdout=acklink_throughput, stderr=PIPE)
-    acklink_results = proc.communicate()[1]
-    sys.stderr.write(acklink_results)
-    stats.write(acklink_results)
-
-    acklink_throughput.close()
-
-    # delay
-    acklink_delay = open(acklink_delay_svg, 'w')
-    cmd = [delay_cmd, acklink_log]
-
-    sys.stderr.write('+ ' + ' '.join(cmd) + '\n')
-    proc = Popen(cmd, stdout=acklink_delay, stderr=DEVNULL)
-    proc.communicate()
-
-    acklink_delay.close()
 
     stats.close()
 
