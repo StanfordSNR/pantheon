55d249332d5c74f352a2f36b6d8a34dd280c2086
Francis Y. Yan <francisyyan@stanford.edu>
2016-10-31 23:59:47 -0700

    parse_arguments.py: add default values in usage

--- test/parse_arguments.py
+++ test/parse_arguments.py
@@ -15,29 +15,32 @@ def parse_arguments(filename):
     if filename == 'test.py' or filename == 'run.py':
         parser.add_argument(
             '-f', action='store', dest='flows', type=int, default=1,
-            help='number of flows (mm-tunnelclient/mm-tunnelserver pairs)')
+            help='number of flows (mm-tunnelclient/mm-tunnelserver pairs, '
+                 'default 1)')
         parser.add_argument(
             '-t', action='store', dest='runtime', type=int, default=60,
-            help='total runtime of test')
+            help='total runtime of test (default 60)')
         parser.add_argument(
-            '--interval', action='store', dest='interval', type=int,
-            default=0, help='interval time between two consecutive flows')
+            '--interval', action='store', dest='interval', type=int, default=0,
+            help='interval in seconds between two flows (default 0)')
         parser.add_argument(
             '--server-side', action='store', dest='server_side',
             choices=['local', 'remote'], default='remote',
-            help='the side on which to run mm-tunnelserver')
+            help='the side to run mm-tunnelserver on (default "remote")')
+        parser.add_argument(
+            '--local-addr', action='store', dest='local_addr', metavar='ADDR',
+            help='local address (IP/hostname that can be reached by the other '
+            'side if --server-side=local')
         parser.add_argument(
             '--sender-side', action='store', dest='sender_side',
             choices=['local', 'remote'], default='local',
-            help='the side to be data sender')
+            help='the side to be data sender (default "local")')
         parser.add_argument(
             '--server-interface', action='store', dest='server_if',
-            metavar='INTERFACE', help='the interface on which to run '
-            'mm-tunnelserver')
+            metavar='INTERFACE', help='interface to run mm-tunnelserver on')
         parser.add_argument(
             '--client-interface', action='store', dest='client_if',
-            metavar='INTERFACE', help='the interface on which to run '
-            'mm-tunnelclient')
+            metavar='INTERFACE', help='interface to run mm-tunnelclient on')
 
     if filename == 'setup.py' or filename == 'test.py':
         parser.add_argument('cc', metavar='congestion-control',
@@ -46,10 +49,12 @@ def parse_arguments(filename):
         group = parser.add_mutually_exclusive_group()
         group.add_argument(
             '--test-only', action='store_true', dest='test_only',
-            default=False, help='run test only without running setup')
+            default=False, help='run test only without running setup '
+            '(default False)')
         group.add_argument(
             '--setup-only', action='store_true', dest='setup_only',
-            default=False, help='run setup only without running test')
+            default=False, help='run setup only without running test '
+            '(default False)')
 
     args = parser.parse_args()
 
@@ -68,5 +73,10 @@ def parse_arguments(filename):
         if args.private_key or args.server_if or args.client_if:
             assert args.remote, ('-i, --server-interface, --client-interface '
                                  'must run along with -r')
+
+        if args.server_side == 'local':
+            assert args.local_addr, (
+                'Must provide local address that can be reached by the other '
+                'side if --server-side=local')
 
     return args
