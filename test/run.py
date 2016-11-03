655e8fa5b45e4e107fe5573089bd3e9bb0cd4e99
Francis Y. Yan <francisyyan@stanford.edu>
2016-11-02 20:52:28 -0700

    Fix a bug (args.remote) in run.py

--- test/run.py
+++ test/run.py
@@ -30,7 +30,7 @@ def create_metadata_file(args, metadata_fname):
         metadata_file.write('local_address=%s\n' % args.local_addr)
 
     if args.remote:
-        metadata_file.write('remote_address=%s\n' % remote.split(':')[0])
+        metadata_file.write('remote_address=%s\n' % args.remote.split(':')[0])
 
     metadata_file.close()
 
