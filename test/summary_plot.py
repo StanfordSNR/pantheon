e3528725ac7dddba54f92e67b504560b63da14bf
Francis Y. Yan <francisyyan@stanford.edu>
2016-11-05 16:39:51 -0700

    Refine summary plot

--- test/summary_plot.py
+++ test/summary_plot.py
@@ -14,7 +14,7 @@ import matplotlib.colors as colors
 import matplotlib.markers as markers
 
 
-def find_throughput_delay(log_name):
+def get_delay_throughput(log_name):
     stats_log = open(log_name)
     throughput = None
     delay = None
@@ -33,33 +33,44 @@ def find_throughput_delay(log_name):
             break
 
     stats_log.close()
-    return (throughput, delay)
+    return (delay, throughput)
 
 
 def plot_summary(data, pretty_names, pantheon_summary_png):
+    min_delay = None
     color_i = 0
     marker_i = 0
     color_names = colors.cnames.keys()
     marker_names = markers.MarkerStyle.filled_markers
+
     for cc, value in data.items():
         cc_name = pretty_names[cc]
         color = color_names[color_i]
         marker = marker_names[marker_i]
-        plt.scatter(*zip(*value), color=color, marker=marker, label=cc_name)
+        x_data, y_data = zip(*value)
+        plt.scatter(x_data, y_data, color=color, marker=marker, label=cc_name)
         color_i = color_i + 1 if color_i < len(color_names) - 1 else 0
         marker_i = marker_i + 1 if marker_i < len(marker_names) - 1 else 0
 
-    # avoid negative throughput values on y axis
-    axes = plt.gca()
-    ymin = axes.get_ylim()[0]
-    if ymin < 0:
-        axes.set_ylim(bottom=0)
+        cc_min_delay = min(x_data)
+        if not min_delay or cc_min_delay < min_delay:
+            min_delay = cc_min_delay
 
-    plt.legend(scatterpoints=1)
+    axes = plt.gca()
+
+    xticks = axes.get_xticks()
+    if min_delay >= 0 and xticks[0] <= 0:
+        x_interval = xticks[1] - xticks[0]
+        xmin = -x_interval / 5
+        axes.set_xlim(left=xmin)
+
+    plt.legend(scatterpoints=1, bbox_to_anchor=(1, 0.5), loc='center left')
     plt.xlabel('95th percentile of per-packet one-way delay (ms)')
     plt.ylabel('Average throughput (Mbit/s)')
     plt.title('Summary of results')
-    plt.savefig(pantheon_summary_png, dpi=300)
+    plt.grid()
+    plt.savefig(pantheon_summary_png, dpi=300,
+                bbox_inches='tight', pad_inches=0.2)
 
 
 def main():
@@ -81,7 +92,7 @@ def main():
 
         for run_id in xrange(1, 1 + args.run_times):
             log_name = path.join(test_dir, '%s_stats_run%s.log' % (cc, run_id))
-            data[cc].append(find_throughput_delay(log_name))
+            data[cc].append(get_delay_throughput(log_name))
 
     plot_summary(data, pretty_names, pantheon_summary_png)
 
