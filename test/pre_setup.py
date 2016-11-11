e04cedd4c317c6b7043a642c50ee9f70f2139626
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-10 18:31:36 -0800

    pre_setup.py add ntp to programs to install

--- test/pre_setup.py
+++ test/pre_setup.py
@@ -34,7 +34,7 @@ class TestPreSetup(unittest.TestCase):
             check_call(cmd, shell=True)
 
         # install texlive, matplotlib, etc.
-        cmd = 'sudo apt-get -yq --force-yes install texlive python-matplotlib'
+        cmd = 'sudo apt-get -yq --force-yes install texlive python-matplotlib ntp'
         check_call(cmd, shell=True)
 
         # install mahimahi
