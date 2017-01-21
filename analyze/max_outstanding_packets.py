a784d13f5021c2e1f60a67c4ce1ea621dad7ce25
Greg D. Hill <greghill@cs.stanford.edu>
2017-01-20 16:36:32 -0800

    fix bug in max_outstanding_packets.py

--- analyze/max_outstanding_packets.py
+++ analyze/max_outstanding_packets.py
@@ -18,16 +18,16 @@ with open(sys.argv[1]) as tunlog:
 
         words = line.split()
         timestamp = int(round(float(words[0])))
-        if len(words) == 4:
-            assert words[1] == '+'
+        if len(words) == 5:
+            assert words[1] == '-'
             prop_delay = int(round(float(words[3])))
             for i in range(timestamp-prop_delay, timestamp):
                 if i in outstanding_packets:
                     outstanding_packets[i] += 1
                 else:
                     outstanding_packets[i] = 1
-        elif len(words) == 5:
-            assert words[1] == '-'
+        elif len(words) == 4:
+            assert words[1] == '+'
         else:
             assert False, line
     print max(outstanding_packets.itervalues())
