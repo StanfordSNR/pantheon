9e30273ff3ff4758991405af0876fa4254e66fcb
Greg D. Hill <greghill@cs.stanford.edu>
2017-02-07 13:43:11 -0600

    binned log records loss rate

--- analyze/greg-bin-logs.py
+++ analyze/greg-bin-logs.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+import math
 import numpy as np
 
 
@@ -19,34 +20,47 @@ def parse_line(line):
     (ts, prop_delay, size) = line.split()
     return (float(ts), float(prop_delay), int(size))
 
+def write_out_bin(outfile, ms_per_bin, packet_bin, bin_bytes, num_lost):
+    bin_megabits = bin_bytes * 8. / (1024. * 1024.)
+    bin_mbps = bin_megabits * 1000. / ms_per_bin
+
+    loss_rate = float(num_lost)/float(num_lost + len(packet_bin))
+    outfile.write('%.3f %.3f %.3f\n' % (np.mean(packet_bin), bin_mbps, loss_rate))
 
 def main():
     args = parse_arguments()
 
     prev_bin_idx = -1
     latest_bin = []
+    latest_num_lost = 0
     latest_bin_bytes = 0
     with open(args.input_log_name) as input_log, open(args.output_log_name, 'w') as output_log:
         for line in input_log:
             (ts, prop_delay, size) = parse_line(line)
 
             bin_idx = int(ts / args.ms_per_bin)
-            if bin_idx == prev_bin_idx:
-                latest_bin.append(prop_delay)
-                latest_bin_bytes += size
-            else:
+            if bin_idx != prev_bin_idx:
+                # write out bin
                 if prev_bin_idx >= 0:
-                    bin_megabits = latest_bin_bytes * 8. / (1024. * 1024.)
-                    bin_mbps = bin_megabits * 1000. / args.ms_per_bin
-                    output_log.write('%.3f %.3f\n' % (np.mean(latest_bin), bin_mbps))
-
-                latest_bin = [prop_delay]
-                latest_bin_bytes = size
+                    write_out_bin(output_log, args.ms_per_bin, latest_bin, latest_bin_bytes, latest_num_lost)
+                    latest_bin = []
+                    latest_num_lost = 0
+                    latest_bin_bytes = 0
                 if bin_idx - prev_bin_idx > 1:
                     print bin_idx - prev_bin_idx
 
+
+            if math.isnan(prop_delay):
+                latest_num_lost += 1
+            else:
+                latest_bin.append(prop_delay)
+                latest_bin_bytes += size
+
+
             prev_bin_idx = bin_idx
-                
+
+        # can skip last bin
+
 
 if __name__ == '__main__':
     main()
