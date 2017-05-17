20de5b0caa024fa4b53b0fece710a6323a6755af
Francis Y. Yan <fyy@cs.stanford.edu>
2017-05-17 11:06:48 -0700

    Rewrite test/setup.py: separate --install and --build

--- test/setup.py
+++ test/setup.py
@@ -1,72 +1,46 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import pantheon_helpers
 from os import path
-from helpers.parse_arguments import parse_arguments
-from helpers.pantheon_help import (call, check_call, check_output,
-                                   parse_remote, sanity_check_gitmodules)
+import project_root
+from helpers.helpers import (
+    parse_arguments, check_call, check_output, update_submodules)
 
 
-class Setup:
-    def __init__(self, args):
-        self.cc = args.cc.lower()
-        self.remote = args.remote
-        self.test_dir = path.abspath(path.dirname(__file__))
-        sanity_check_gitmodules()
+def install_deps(cc_src):
+    cmd = ['python', cc_src, 'deps']
+    deps = check_output(cmd).strip()
 
-    def install(self):
-        cmd = ['python', self.src_file, 'deps']
-        deps = check_output(cmd).strip()
+    if deps:
+        cmd = 'sudo apt-get -y install ' + deps
+        check_call(cmd, shell=True)
 
-        if deps:
-            sys.stderr.write('Installing dependencies...\n')
-            cmd = 'sudo apt-get -yq --force-yes install ' + deps
-            check_call(cmd, shell=True)
 
-    def build(self):
-        sys.stderr.write('Building...\n')
-        cmd = ['python', self.src_file, 'build']
-        check_call(cmd)
+def setup(args):
+    update_submodules()
 
-    def initialize(self):
-        sys.stderr.write('Performing intialization commands...\n')
-        cmd = ['python', self.src_file, 'init']
-        check_call(cmd)
+    # enable IP forwarding
+    cmd = 'sudo sysctl -w net.ipv4.ip_forward=1'
+    check_call(cmd, shell=True)
 
-    def setup_congestion_control(self):
-        src_dir = path.abspath(path.join(self.test_dir, '../src'))
-        self.src_file = path.join(src_dir, self.cc + '.py')
+    # setup specified schemes
+    for cc in args.cc_schemes:
+        cc_src = path.join(project_root.DIR, 'src', cc + '.py')
 
-        # get build dependencies
-        self.install()
+        if args.install_deps:
+            # install dependencies
+            install_deps(cc_src)
 
-        # run build commands
-        self.build()
+        if args.build:
+            # build the scheme
+            check_call(['python', cc_src, 'build'])
 
-        # run initialize commands
-        self.initialize()
-
-    # congestion control setup
-    def setup(self):
-        self.setup_congestion_control()
-
-        # run remote setup.py
-        if self.remote:
-            rd = parse_remote(self.remote)
-            cmd = rd['ssh_cmd'] + ['python', rd['setup'], self.cc]
-            check_call(cmd)
+        check_call(['python', cc_src, 'init'])
 
 
 def main():
-    args = parse_arguments(path.basename(__file__))
-
-    setup = Setup(args)
-    setup.setup()
+    args = parse_arguments(path.abspath(__file__))
+    setup(args)
 
 
 if __name__ == '__main__':
-    DEVNULL = open(os.devnull, 'w')
     main()
-    DEVNULL.close()
