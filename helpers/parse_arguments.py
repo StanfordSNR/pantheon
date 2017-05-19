6dbcec4953656c0e4cec48d545e8f40bd2a81e7f
Francis Y. Yan <fyy@cs.stanford.edu>
2017-05-18 23:11:54 -0700

    Add more sanity checks in helpers/parse_arguments.py
    Improve verus.py's tmp output

--- helpers/parse_arguments.py
+++ helpers/parse_arguments.py
@@ -5,7 +5,7 @@ import yaml
 import project_root
 
 
-def build_arg_dict():
+def build_arg_dict(cc_schemes):
     arg_dict = {}
 
     arg_dict['-r'] = {
@@ -128,10 +128,6 @@ def build_arg_dict():
         'help': 'run times of each test (default 1)',
     }
 
-    with open(path.join(project_root.DIR, 'src', 'config.yml')) as config:
-        cfg = yaml.load(config)
-    cc_schemes = ' '.join(cfg.keys())
-
     arg_dict['--schemes'] = {
         'metavar': '"SCHEME_1 SCHEME_2..."',
         'default': cc_schemes,
@@ -235,11 +231,13 @@ def add_arg_list(parser, arg_dict, arg_list):
         parser.add_argument(arg, **arg_dict[arg])
 
 
-def validate_args(args):
+def validate_args(args, cc_schemes_list):
+    cc = getattr(args, 'cc', None)
     remote = getattr(args, 'remote', None)
     flows = getattr(args, 'flows', None)
     runtime = getattr(args, 'runtime', None)
     interval = getattr(args, 'interval', None)
+    run_id = getattr(args, 'run_id', None)
     server_side = getattr(args, 'server_side', None)
     local_addr = getattr(args, 'local_addr', None)
     sender_side = getattr(args, 'sender_side', None)
@@ -249,6 +247,10 @@ def validate_args(args):
     append_mm_cmds = getattr(args, 'append_mm_cmds', None)
     extra_mm_link_args = getattr(args, 'extra_mm_link_args', None)
 
+    if cc is not None:
+        if cc not in cc_schemes_list:
+            sys.exit('%s is not listed in src/config.yml' % cc)
+
     if remote_if is not None:
         if remote is None:
             sys.exit('--remote-interface must run along with -r')
@@ -257,21 +259,31 @@ def validate_args(args):
         if remote is None:
             sys.exit('--remote-info must run along with -r')
 
-    if remote:
+    if remote is not None:
         if ':' not in remote:
             sys.exit('-r must be followed by [user@]hostname:dir')
 
         if flows < 1:
             sys.exit('remote test must run at least one flow')
 
-    if runtime:
-        if runtime > 60:
-            sys.exit('runtime cannot be greater than 60 seconds')
+    if runtime is not None:
+        if runtime > 60 or runtime <= 0:
+            sys.exit('runtime cannot be non-positive or greater than 60 s')
 
-        if flows and interval:
+    if flows is not None and flows < 0:
+        sys.exit('flow cannot be negative')
+
+    if interval is not None and interval < 0:
+        sys.exit('interval cannot be negative')
+
+    if flows is not None and interval is not None:
+        if flows > 0 and interval > 0:
             if (flows - 1) * interval > runtime:
                 sys.exit('interval time between flows is too long to be '
                          'fit in runtime')
+
+    if run_id is not None and run_id <= 0:
+        sys.exit('run_id must be positive')
 
     if server_side == 'local':
         if local_addr is None:
@@ -283,7 +295,7 @@ def validate_args(args):
             sys.exit('local test can only run tunnel server and sender '
                      'inside mm-link')
 
-    if remote:
+    if remote is not None:
         if prepend_mm_cmds is not None:
             sys.exit('--prepend-mm-cmds can\'t be run with -r')
 
@@ -295,15 +307,18 @@ def validate_args(args):
 
 
 def parse_arguments(file_path):
+    with open(path.join(project_root.DIR, 'src', 'config.yml')) as config:
+        cfg = yaml.load(config)
+    cc_schemes_list = cfg.keys()
+    cc_schemes = ' '.join(cc_schemes_list)
+
     parser = argparse.ArgumentParser()
-    arg_dict = build_arg_dict()
+    arg_dict = build_arg_dict(cc_schemes)
 
     test_dir = path.join(project_root.DIR, 'test')
     analyze_dir = path.join(project_root.DIR, 'analyze')
 
-    if file_path == path.join(project_root.DIR, 'install_deps.py'):
-        add_arg_list(parser, arg_dict, ['--interface'])
-    elif file_path == path.join(test_dir, 'setup.py'):
+    if file_path == path.join(test_dir, 'setup.py'):
         add_arg_list(parser, arg_dict, [
             '--install-deps', '--build', 'cc_schemes'])
     elif file_path == path.join(test_dir, 'test.py'):
@@ -336,7 +351,7 @@ def parse_arguments(file_path):
             '--include-acklink', '--analyze-schemes'])
 
     args = parser.parse_args()
-    validate_args(args)
+    validate_args(args, cc_schemes_list)
 
     return args
 
@@ -344,9 +359,9 @@ def parse_arguments(file_path):
 def parse_remote(remote, cc=None):
     ret = {}
 
-    ret['host_addr'], ret['pantheon_dir'] = remote.split(':')
-    ret['ip'] = ret['host_addr'].split('@')[-1]
-    ret['ssh_cmd'] = ['ssh', ret['host_addr']]
+    ret['addr'], ret['pantheon_dir'] = remote.split(':')
+    ret['ip'] = ret['addr'].split('@')[-1]
+    ret['ssh_cmd'] = ['ssh', ret['addr']]
 
     ret['src_dir'] = path.join(ret['pantheon_dir'], 'src')
     ret['test_dir'] = path.join(ret['pantheon_dir'], 'test')
