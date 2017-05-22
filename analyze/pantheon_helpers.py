c358ed9e0579f1d44f46880ea7ffd56fae3596ec
Francis Y. Yan <fyy@cs.stanford.edu>
2017-05-22 01:46:45 -0700

    Add analyze_helpers.py and analyze/parse_arguments.py

--- analyze/pantheon_helpers.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os
-from os import path
-sys.path.append(path.abspath(path.join(path.dirname(__file__), os.pardir)))
---
+++ analyze/project_root.py
@@ -0,0 +1,5 @@
+import sys
+import os
+from os import path
+DIR = path.abspath(path.join(path.dirname(path.abspath(__file__)), os.pardir))
+sys.path.append(DIR)
