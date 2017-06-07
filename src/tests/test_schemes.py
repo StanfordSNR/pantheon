cb422b8211c3a613f79505ce9b166967dbd04dec
Francis Y. Yan <fyy@cs.stanford.edu>
2017-06-07 11:57:17 -0700

    Run test_schemes.py on Travis

--- src/tests/test_schemes.py
+++ src/tests/test_schemes.py
@@ -4,10 +4,11 @@ import sys
 import time
 import os
 from os import path
+import signal
 import project_root
 from helpers.helpers import (
     get_open_port, check_output, call, Popen, PIPE, parse_config,
-    kill_proc_group)
+    kill_proc_group, timeout_handler, TimeoutError, get_signal_for_cc)
 
 
 def test_schemes():
@@ -15,6 +16,9 @@ def test_schemes():
     schemes = parse_config().keys()
 
     for scheme in schemes:
+        if scheme == 'bbr' or scheme == 'quic':  # skip them on Travis
+            continue
+
         sys.stderr.write('Testing %s...\n' % scheme)
         src = path.join(src_dir, scheme + '.py')
 
@@ -34,12 +38,25 @@ def test_schemes():
         cmd = [src, run_second, '127.0.0.1', port]
         second_proc = Popen(cmd, preexec_fn=os.setsid)
 
-        # test lasts for 3 seconds
-        time.sleep(3)
+        # test lasts for 5 seconds
+        signal.signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(5)
 
-        # cleanup
-        kill_proc_group(first_proc)
-        kill_proc_group(second_proc)
+        try:
+            for proc in [first_proc, second_proc]:
+                proc.wait()
+                if proc.returncode != 0:
+                    sys.exit('%s failed in tests\n' % scheme)
+        except TimeoutError:
+            pass
+        else:
+            signal.alarm(0)
+            sys.exit('test exited before time limit')
+        finally:
+            # cleanup
+            signum = get_signal_for_cc(scheme)
+            kill_proc_group(first_proc, signum)
+            kill_proc_group(second_proc, signum)
 
 
 def cleanup():
