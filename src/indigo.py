929dd66698d1ee42f6e3a32520cbb132f93a0c2e
Francis Y. Yan <fyy@cs.stanford.edu>
2017-08-27 18:29:25 -0700

    Add Indigo (LSTM)

---
+++ src/indigo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from os import path
+from subprocess import check_call
+from src_helpers import parse_arguments, check_default_qdisc
+import project_root
+
+
+def main():
+    args = parse_arguments('sender_first')
+
+    cc_repo = path.join(project_root.DIR, 'third_party', 'indigo')
+    send_src = path.join(cc_repo, 'dagger', 'run_sender.py')
+    recv_src = path.join(cc_repo, 'env', 'run_receiver.py')
+
+    if args.option == 'run_first':
+        print 'sender'
+
+    if args.option == 'setup':
+        check_call(['sudo pip install tensorflow'], shell=True)
+
+    if args.option == 'setup_after_reboot':
+        check_default_qdisc('indigo')
+
+    if args.option == 'sender':
+        cmd = [send_src, args.port]
+        check_call(cmd)
+
+    if args.option == 'receiver':
+        cmd = [recv_src, args.ip, args.port]
+        check_call(cmd)
+
+
+if __name__ == '__main__':
+    main()
