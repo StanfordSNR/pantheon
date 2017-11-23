a0e8d84428e070a9090d1329bc16646e70119c3a
fillthepipe <33445015+fillthepipe@users.noreply.github.com>
2017-11-23 15:35:50 +0800

    Update fillp.py

--- src/fillp.py
+++ src/fillp.py
@@ -25,11 +25,11 @@ def main():
     cc_repo = path.join(project_root.DIR, 'third_party', 'fillp')
     send_src = path.join(cc_repo, 'client', 'client')
     recv_src = path.join(cc_repo, 'server', 'server')
-    output = commands.getstatusoutput("sysctl net.ipv4.udp_mem |awk -F '=' '{print $2}'")  
+    output = getstatusoutput("sysctl net.ipv4.udp_mem |awk -F '=' '{print $2}'")  
     udp_men=output[1].strip().split()
     orgin_udp_men_min = int(udp_men[0])
     orgin_udp_men_default = int(udp_men[1])
-    orgin_udp_men_max = int(oudp_menu[2])
+    orgin_udp_men_max = int(udp_menu[2])
    
     
     if args.option == 'deps':
