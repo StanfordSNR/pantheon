ab700d994663909f50d0156576358a967666cbcd
Francis Y. Yan <fyy@cs.stanford.edu>
2017-12-17 18:33:41 -0800

    Fix fillp.py (skip running it on Travis due to file permission error)

--- src/fillp.py
+++ src/fillp.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
- 
+
 import sys
 import os
 from os import path
 import string
 import random
 import shutil
-from subprocess import (check_call, Popen)
+from subprocess import check_call
 from src_helpers import (parse_arguments, make_sure_path_exists,
                          check_default_qdisc)
 import project_root
-    
+
 
 def main():
     args = parse_arguments('receiver_first')
@@ -21,21 +21,21 @@ def main():
     recv_path = path.join(cc_repo, 'server')
 
     if args.option == 'run_first':
-         print 'receiver'
- 
+        print 'receiver'
+
     if args.option == 'setup_after_reboot':
-         check_default_qdisc('fillp')
- 
+        check_default_qdisc('fillp')
+
     if args.option == 'sender':
         os.environ['LD_LIBRARY_PATH'] = send_path
         cmd = [send_src, '-d', args.ip, '-p', args.port, '-t']
-        Popen(cmd)
+        check_call(cmd)
 
     if args.option == 'receiver':
         os.environ['LD_LIBRARY_PATH'] = recv_path
-        cmd = [recv_src, '-s', '0.0.0.0', '-p', args.port, '-t']        
-        Popen(cmd)
+        cmd = [recv_src, '-s', '0.0.0.0', '-p', args.port, '-t']
+        check_call(cmd)
 
-        
+
 if __name__ == '__main__':
     main()
