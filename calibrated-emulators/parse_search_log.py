9044090a08330fdd8bf47b6556635de1d7f9ef81
Francis Y. Yan <fyy@cs.stanford.edu>
2018-01-19 23:16:20 -0800

    Edit parse_search_log

--- calibrated-emulators/parse_search_log.py
+++ calibrated-emulators/parse_search_log.py
@@ -10,10 +10,10 @@ def main():
 
     search_log = open(args.search_log)
 
-    min_loss = 100.0
+    min_loss = 500.0
     min_items = None
 
-    sec_loss = 100.0
+    sec_loss = 500.0
     sec_items = None
 
     below_loss = 30.0
@@ -26,42 +26,50 @@ def main():
 
         items = line.split(',')
 
-        bw = items[0].split('=')[1]
-        delay = items[1].split('=')[1]
-        uplink_queue = items[2].split('=')[1]
-        #uplink_loss = items[3].split('=')[1]
-        #downlink_loss = items[4].split('=')[1]
-        tput_loss = items[3].split('=')[1]
-        delay_loss = items[4].split('=')[1]
-        overall_loss = items[5].split('=')[1]
+        col = 0
+        bw = items[col].split('=')[1]
+        col += 1
+        delay = items[col].split('=')[1]
+        col += 1
+        uplink_queue = items[col].split('=')[1]
+        col += 1
+        uplink_loss = items[col].split('=')[1]
+        #col += 1
+        #downlink_loss = items[col].split('=')[1]
+        col += 1
+        tput_loss = items[col].split('=')[1]
+        col += 1
+        delay_loss = items[col].split('=')[1]
+        col += 1
+        overall_loss = items[col].split('=')[1]
 
         if float(overall_loss) < min_loss:
             old_min_loss = min_loss
             old_min_items = min_items
 
             min_loss = float(overall_loss)
-            min_items = (bw, delay, uplink_queue, #uplink_loss, downlink_loss,
+            min_items = (bw, delay, uplink_loss, uplink_queue, #uplink_loss, downlink_loss,
                          tput_loss, delay_loss, overall_loss)
 
             sec_loss = old_min_loss
             sec_items = old_min_items
         elif float(overall_loss) < sec_loss:
             sec_loss = float(overall_loss)
-            sec_items = (bw, delay, uplink_queue, #uplink_loss, downlink_loss,
+            sec_items = (bw, delay, uplink_loss, uplink_queue, #uplink_loss, downlink_loss,
                          tput_loss, delay_loss, overall_loss)
 
-        if float(overall_loss) < 30.0:
-            below_items.append((bw, delay, uplink_queue, tput_loss, delay_loss, overall_loss))
+        #if float(overall_loss) < 300.0:
+        #    below_items.append((bw, delay, uplink_queue, tput_loss, delay_loss, overall_loss))
 
     search_log.close()
 
     # print 'bw, delay, uplink_queue, uplink_loss, downlink_loss, tput_loss, delay_loss, overall_loss'
-    print 'bw, delay, uplink_queue, tput_loss, delay_loss, overall_loss'
+    print 'bw, delay, uplink_loss, uplink_queue, tput_loss, delay_loss, overall_loss'
     print min_items
     print sec_items
 
-    for item in below_items:
-        print item
+    #for item in below_items:
+    #    print item
 
 
 if __name__ == '__main__':
