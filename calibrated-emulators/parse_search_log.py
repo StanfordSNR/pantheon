240367eb479914a949aeb58e952c647fce3a34c0
Francis Y. Yan <fyy@cs.stanford.edu>
2018-01-25 08:42:31 -0800

    Improve parse_search_log.py

--- calibrated-emulators/parse_search_log.py
+++ calibrated-emulators/parse_search_log.py
@@ -6,7 +6,12 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('search_log')
+    parser.add_argument('order')
     args = parser.parse_args()
+
+    order = args.order.split(',')
+    for i in range(len(order)):
+        order[i] = order[i].strip()
 
     search_log = open(args.search_log)
 
@@ -16,9 +21,6 @@ def main():
     sec_loss = 500.0
     sec_items = None
 
-    below_loss = 30.0
-    below_items = []
-
     while True:
         line = search_log.readline()
         if not line:
@@ -27,49 +29,47 @@ def main():
         items = line.split(',')
 
         col = 0
-        bw = items[col].split('=')[1]
-        col += 1
-        delay = items[col].split('=')[1]
-        col += 1
-        uplink_queue = items[col].split('=')[1]
-        col += 1
-        uplink_loss = items[col].split('=')[1]
-        #col += 1
-        #downlink_loss = items[col].split('=')[1]
-        col += 1
+
+        candidate_items = []
+        for x in order:
+            if x == 'bandwidth':
+                candidate_items.append(items[col].split('=')[1])
+            elif x == 'delay':
+                candidate_items.append(items[col].split('=')[1])
+            elif x == 'uplink_loss':
+                candidate_items.append(items[col].split('=')[1])
+            elif x == 'uplink_queue':
+                candidate_items.append(items[col].split('=')[1])
+
+            col += 1
+
         tput_loss = items[col].split('=')[1]
+        candidate_items.append(tput_loss)
         col += 1
         delay_loss = items[col].split('=')[1]
+        candidate_items.append(delay_loss)
         col += 1
         overall_loss = items[col].split('=')[1]
+        candidate_items.append(overall_loss)
 
         if float(overall_loss) < min_loss:
             old_min_loss = min_loss
             old_min_items = min_items
 
             min_loss = float(overall_loss)
-            min_items = (bw, delay, uplink_loss, uplink_queue, #uplink_loss, downlink_loss,
-                         tput_loss, delay_loss, overall_loss)
+            min_items = candidate_items
 
             sec_loss = old_min_loss
             sec_items = old_min_items
         elif float(overall_loss) < sec_loss:
             sec_loss = float(overall_loss)
-            sec_items = (bw, delay, uplink_loss, uplink_queue, #uplink_loss, downlink_loss,
-                         tput_loss, delay_loss, overall_loss)
-
-        #if float(overall_loss) < 300.0:
-        #    below_items.append((bw, delay, uplink_queue, tput_loss, delay_loss, overall_loss))
+            sec_items = candidate_items
 
     search_log.close()
 
-    # print 'bw, delay, uplink_queue, uplink_loss, downlink_loss, tput_loss, delay_loss, overall_loss'
-    print 'bw, delay, uplink_loss, uplink_queue, tput_loss, delay_loss, overall_loss'
+    print args.order + ", tput_loss, delay_loss, overall_loss"
     print min_items
     print sec_items
-
-    #for item in below_items:
-    #    print item
 
 
 if __name__ == '__main__':
