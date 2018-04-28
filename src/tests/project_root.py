77034a3cd18137ca5050d400c951396f33a2f44e
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-28 16:36:43 -0700

    Restructure the layout

--- src/tests/project_root.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-from os import path
-DIR = path.abspath(path.join(path.dirname(path.abspath(__file__)),
-                             os.pardir, os.pardir))
-sys.path.append(DIR)
--- src/tools/install_deps.sh
+++ src/tools/install_deps.sh
@@ -1,15 +1,12 @@
 #!/bin/sh -x
 
-# update submodules
-git submodule update --init --recursive
-
 # update mahimahi source line and package listings
 sudo add-apt-repository -y ppa:keithw/mahimahi
 sudo apt-get update
 
 # install required packages
 sudo apt-get -y install mahimahi ntp ntpdate texlive python-pip
-sudo pip install matplotlib numpy tabulate pyyaml
+pip install --user matplotlib numpy tabulate pyyaml
 
 # install pantheon tunnel
 sudo apt-get -y install debhelper autotools-dev dh-autoreconf iptables \
