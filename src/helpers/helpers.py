ff4d79310fdcfc26fbeee424c7a4308675b72f8f
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-29 16:39:13 -0700

    Separate subprocess wrappers to helpers/subprocess_wrappers.py

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -33,38 +33,6 @@ TMPDIR = path.join(tempfile.gettempdir(), 'pantheon-tmp')
 make_sure_path_exists(TMPDIR)
 
 
-def print_cmd(cmd):
-    if isinstance(cmd, list):
-        cmd_to_print = ' '.join(cmd).strip()
-    elif isinstance(cmd, (str, unicode)):
-        cmd_to_print = cmd.strip()
-    else:
-        cmd_to_print = ''
-
-    if cmd_to_print:
-        sys.stderr.write('$ %s\n' % cmd_to_print)
-
-
-def call(cmd, **kwargs):
-    print_cmd(cmd)
-    return subprocess.call(cmd, **kwargs)
-
-
-def check_call(cmd, **kwargs):
-    print_cmd(cmd)
-    return subprocess.check_call(cmd, **kwargs)
-
-
-def check_output(cmd, **kwargs):
-    print_cmd(cmd)
-    return subprocess.check_output(cmd, **kwargs)
-
-
-def Popen(cmd, **kwargs):
-    print_cmd(cmd)
-    return subprocess.Popen(cmd, **kwargs)
-
-
 def parse_config():
     with open(path.join(project_root.DIR, 'src', 'config.yml')) as config:
         return yaml.load(config)
