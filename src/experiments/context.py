03f91de72a56d6a6bb2c1c8dc7f63f369f107d8c
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-29 17:46:31 -0700

    Replace project_root with context

---
+++ src/experiments/context.py
@@ -0,0 +1,4 @@
+import os
+import sys
+SRCDIR=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, SRCDIR)
--- src/experiments/project_root.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-from os import path
-DIR = path.abspath(path.join(path.dirname(path.abspath(__file__)), os.pardir))
-sys.path.append(DIR)
