fe1c1c0f86831d3db67928c3c8b481ab4209ed55
Francis Y. Yan <fyy@cs.stanford.edu>
2018-05-04 12:33:28 -0700

    Update vivace submodule that allows for specifying port

--- src/wrappers/vivace.py
+++ src/wrappers/vivace.py
@@ -14,18 +14,18 @@ def main():
     cc_repo = path.join(context.third_party_dir, 'vivace')
     recv_dir = path.join(cc_repo, 'receiver')
     send_dir = path.join(cc_repo, 'sender')
-    recv_src = path.join(recv_dir, 'appserver')
-    send_src = path.join(send_dir, 'gradient_descent_pcc_client')
+    recv_src = path.join(recv_dir, 'vivace_receiver')
+    send_src = path.join(send_dir, 'vivace_sender')
 
     if args.option == 'receiver':
         os.environ['LD_LIBRARY_PATH'] = path.join(recv_dir)
-        cmd = [recv_src]  # TODO: remove the fixed port 9000
+        cmd = [recv_src, args.port]
         check_call(cmd)
         return
 
     if args.option == 'sender':
         os.environ['LD_LIBRARY_PATH'] = path.join(send_dir)
-        cmd = [send_src, args.ip, '9000', '1']
+        cmd = [send_src, args.ip, args.port]
         check_call(cmd)
         return
 
